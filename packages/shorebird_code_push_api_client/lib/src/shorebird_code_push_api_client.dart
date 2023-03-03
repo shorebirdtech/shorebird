c5ed30f5ec82d1128afc21d6c144612b534ba8ae
Felix Angelov <felix@shorebird.dev>
2023-03-03 09:52:16 -0600

    feat(shorebird_code_push_api_client): add `createRelease` (#5)

--- packages/shorebird_code_push_api_client/lib/src/shorebird_code_push_api_client.dart
+++ packages/shorebird_code_push_api_client/lib/src/shorebird_code_push_api_client.dart
@@ -1,7 +1,33 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
 /// {@template shorebird_code_push_api_client}
 /// The Shorebird CodePush API Client
 /// {@endtemplate}
 class ShorebirdCodePushApiClient {
   /// {@macro shorebird_code_push_api_client}
-  const ShorebirdCodePushApiClient();
+  ShorebirdCodePushApiClient({http.Client? httpClient, Uri? hostedUri})
+      : _httpClient = httpClient ?? http.Client(),
+        _hostedUri = hostedUri ??
+            Uri.https('shorebird-code-push-api-cypqazu4da-uc.a.run.app');
+
+  final http.Client _httpClient;
+  final Uri _hostedUri;
+
+  /// Upload the artifact at [path] to the
+  /// Shorebird CodePush API as a new release.
+  Future<void> createRelease(String path) async {
+    final request = http.MultipartRequest(
+      'POST',
+      Uri.parse('$_hostedUri/api/v1/releases'),
+    );
+    final file = await http.MultipartFile.fromPath('file', path);
+    request.files.add(file);
+    final response = await _httpClient.send(request);
+
+    if (response.statusCode != HttpStatus.created) {
+      throw Exception('${response.statusCode} ${response.reasonPhrase}');
+    }
+  }
 }
