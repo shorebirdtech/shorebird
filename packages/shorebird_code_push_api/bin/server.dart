9fe8b143a06500e33be77d39d57c8ff0a97ae317
Felix Angelov <felix@shorebird.dev>
2023-03-06 11:42:26 -0600

    logging

--- packages/shorebird_code_push_api/bin/server.dart
+++ packages/shorebird_code_push_api/bin/server.dart
@@ -16,19 +16,15 @@ Future<void> main() async {
     ..post('/api/v1/releases', uploadReleaseHandler)
     ..get('/api/v1/engines/<revision>', downloadEngineHandler);
 
-  print(Platform.environment['CODE_PUSH_API_KEYS'] ?? '[]');
-  print(Platform.environment['GCP_SA'] ?? '');
+  final apiKeys = json.decode(Platform.environment['CODE_PUSH_API_KEYS']!);
+  print(apiKeys.runtimeType);
 
-  final apiKeys = json.decode(
-    Platform.environment['CODE_PUSH_API_KEYS'] ?? '[]',
-  ) as List;
-
-  final gcpKey = Platform.environment['GCP_SA'] ?? '';
+  final gcpKey = Platform.environment['GCP_SA']!;
 
   final handler = const Pipeline()
       .addMiddleware(versionStoreProvider)
       .addMiddleware(httpClientProvider(gcpKey))
-      .addMiddleware(apiKeyVerifier(keys: apiKeys.cast<String>()))
+      .addMiddleware(apiKeyVerifier(keys: (apiKeys as List).cast<String>()))
       .addHandler(router.call);
 
   final server = await shelf_io.serve(
