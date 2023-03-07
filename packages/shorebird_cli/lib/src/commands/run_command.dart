32a567d4be0aa334617ea5922a4d35822ad0901b
Felix Angelov <felix@shorebird.dev>
2023-03-07 13:21:53 -0600

    fix(shorebird_cli): adjust engine file permissions (#36)

--- packages/shorebird_cli/lib/src/commands/run_command.dart
+++ packages/shorebird_cli/lib/src/commands/run_command.dart
@@ -95,6 +95,7 @@ class RunCommand extends Command<int> {
         await _extractShorebirdEngine(
           shorebirdEngineCache.path,
           shorebirdEngine.path,
+          _startProcess,
         );
         buildingEngine.complete();
       } catch (error) {
@@ -150,6 +151,7 @@ Future<void> _downloadShorebirdEngine(
 Future<void> _extractShorebirdEngine(
   String archivePath,
   String targetPath,
+  StartProcess startProcess,
 ) async {
   final targetDirectory = Directory(targetPath);
 
@@ -164,4 +166,26 @@ Future<void> _extractShorebirdEngine(
       extractArchiveToDisk(archive, targetPath);
     },
   );
+
+  // TODO(felangel): support windows and linux
+  // https://github.com/shorebirdtech/shorebird/issues/37
+  // coverage:ignore-start
+  if (Platform.isMacOS) {
+    const executables = [
+      'flutter/prebuilts/macos-x64/dart-sdk/bin/dart',
+      'flutter/prebuilts/macos-x64/dart-sdk/bin/dartaotruntime',
+      'out/android_release_arm64/clang_x64/gen_snapshot',
+      'out/android_release_arm64/clang_x64/gen_snapshot_arm64',
+      'out/android_release_arm64/clang_x64/impellerc',
+    ];
+
+    for (final executable in executables) {
+      final process = await startProcess(
+        'chmod',
+        ['+x', p.join(targetPath, executable)],
+      );
+      await process.exitCode;
+    }
+  }
+  // coverage:ignore-end
 }
