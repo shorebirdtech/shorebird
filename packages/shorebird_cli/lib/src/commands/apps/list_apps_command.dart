f2c42808459a86dcb8e995ee9811798ab8d541e6
Felix Angelov <felix@shorebird.dev>
2023-03-14 14:25:18 -0500

    feat(shorebird_cli): add shorebird apps list

---
+++ packages/shorebird_cli/lib/src/commands/apps/list_apps_command.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+
+import 'package:collection/collection.dart';
+import 'package:mason_logger/mason_logger.dart';
+import 'package:shorebird_cli/src/command.dart';
+import 'package:shorebird_code_push_client/shorebird_code_push_client.dart';
+
+/// {@template list_apps_command}
+///
+/// `shorebird apps list`
+/// List all Shorebird apps.
+/// {@endtemplate}
+class ListAppsCommand extends ShorebirdCommand {
+  /// {@macro list_apps_command}
+  ListAppsCommand({
+    required super.logger,
+    super.buildCodePushClient,
+    super.auth,
+  });
+
+  @override
+  String get description => 'List all Shorebird apps.';
+
+  @override
+  String get name => 'list';
+
+  @override
+  List<String> get aliases => ['ls'];
+
+  @override
+  Future<int>? run() async {
+    final session = auth.currentSession;
+    if (session == null) {
+      logger.err('You must be logged in to publish.');
+      return ExitCode.noUser.code;
+    }
+
+    final client = buildCodePushClient(apiKey: session.apiKey);
+
+    late final List<App> apps;
+    try {
+      apps = await client.getApps();
+    } catch (error) {
+      logger.err('Unable to get apps: $error');
+      return ExitCode.software.code;
+    }
+
+    if (apps.isEmpty) {
+      logger.info('(empty)');
+      return ExitCode.success.code;
+    }
+
+    for (final app in apps) {
+      logger.info(app.prettyPrint());
+    }
+
+    return ExitCode.success.code;
+  }
+}
+
+extension on App {
+  String prettyPrint() {
+    final latestRelease = releases.lastOrNull;
+    final latestPatch = latestRelease?.patches.lastOrNull;
+    final latestReleasePart =
+        latestRelease != null ? 'v${latestRelease.version}' : '(empty)';
+    final latestPatchPart =
+        latestPatch != null ? '(patch #${latestPatch.number})' : '';
+
+    return '$productId: $latestReleasePart $latestPatchPart';
+  }
+}
--- packages/shorebird_code_push_client/lib/shorebird_code_push_client.dart
+++ packages/shorebird_code_push_client/lib/shorebird_code_push_client.dart
@@ -1,4 +1,6 @@
 /// The Shorebird CodePush API Client
 library shorebird_code_push_client;
 
+export 'package:shorebird_code_push_protocol/shorebird_code_push_protocol.dart';
+
 export 'src/code_push_client.dart';
