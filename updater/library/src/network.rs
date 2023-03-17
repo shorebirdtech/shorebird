fe085530d625bda2148586e483ab3ac221813b53
Felix Angelov <felix@shorebird.dev>
2023-03-17 17:44:27 -0500

    refactor(updater): create `PatchCheckRequest` struct (#92)

--- updater/library/src/network.rs
+++ updater/library/src/network.rs
@@ -1,8 +1,7 @@
 // This file's job is to deal with the update_server and network side
 // of the updater library.
 
-use serde::Deserialize;
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
@@ -22,6 +21,17 @@ pub struct Patch {
     pub download_url: String,
 }
 
+#[derive(Debug, Serialize)]
+pub struct PatchCheckRequest {
+    pub app_id: String,
+    pub channel: String,
+    pub release_version: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub patch_number: Option<usize>,
+    pub platform: String,
+    pub arch: String,
+}
+
 #[derive(Debug, Deserialize)]
 pub struct PatchCheckResponse {
     pub patch_available: bool,
@@ -37,21 +47,21 @@ pub fn send_patch_check_request(
 
     // Send the request to the server.
     let client = reqwest::blocking::Client::new();
-    let mut body = HashMap::new();
-    body.insert("app_id", config.app_id.clone());
-    body.insert("channel", config.channel.clone());
-    body.insert("release_version", config.release_version.clone());
-    if let Some(patch) = patch {
-        body.insert("patch_number", patch.number.to_string());
-    }
-    body.insert("platform", current_platform().to_string());
-    body.insert("arch", current_arch().to_string());
-    info!("Sending patch check request: {:?}", body);
+    let req = PatchCheckRequest {
+        app_id: config.app_id.clone(),
+        channel: config.channel.clone(),
+        release_version: config.release_version.clone(),
+        patch_number: patch.map(|p| p.number),
+        platform: current_platform().to_string(),
+        arch: current_arch().to_string(),
+    };
+    info!("Sending patch check request: {:?}", req);
     let response = client
         .post(&patches_check_url(&config.base_url))
-        .json(&body)
+        .json(&req)
         .send()?
         .json()?;
+
     info!("Patch check response: {:?}", response);
     return Ok(response);
 }
@@ -70,4 +80,32 @@ pub fn download_to_path(url: &str, path: &PathBuf) -> anyhow::Result<()> {
     let mut file = File::create(path)?;
     file.write_all(&mut bytes)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::network::PatchCheckResponse;
+
+    #[test]
+    fn check_patch_request_response_deserialization() {
+        let data = r###"
+    {
+        "patch_available": true,
+        "patch": {
+            "number": 1,
+            "download_url": "https://storage.googleapis.com/patch_artifacts/17a28ec1-00cf-452d-bdf9-dbb9acb78600/dlc.vmcode",
+            "hash": "#"
+        }
+    }"###;
+
+        let response: PatchCheckResponse = serde_json::from_str(data).unwrap();
+
+        assert!(response.patch_available == true);
+        assert!(response.patch.is_some());
+        
+        let patch = response.patch.unwrap();
+        assert_eq!(patch.number, 1);
+        assert_eq!(patch.download_url, "https://storage.googleapis.com/patch_artifacts/17a28ec1-00cf-452d-bdf9-dbb9acb78600/dlc.vmcode");
+        assert_eq!(patch.hash, "#");
+    }
 }
