770e4fb2dfb6aed90347d4c1ce77b58a53b8eb73
Felix Angelov <felix@shorebird.dev>
2023-03-22 17:46:18 -0500

    simplify

--- packages/shorebird_cli/test/src/command_runner_test.dart
+++ packages/shorebird_cli/test/src/command_runner_test.dart
@@ -43,12 +43,7 @@ void main() {
       processResult = _MockProcessResult();
       when(() => processResult.exitCode).thenReturn(ExitCode.success.code);
       when(() => processResult.stdout).thenReturn(
-        '''
-Flutter 3.7.8 • channel stable • git@github.com:flutter/flutter.git
-Framework • revision 90c64ed42b (30 hours ago) • 2023-03-21 11:27:08 -0500
-Engine • revision $requiredFlutterEngineRevision
-Tools • Dart 2.19.5 • DevTools 2.20.1
-''',
+        'Engine • revision $requiredFlutterEngineRevision',
       );
 
       commandRunner = ShorebirdCliCommandRunner(
