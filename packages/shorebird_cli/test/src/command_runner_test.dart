40b4a1ae193e619790a421c7a64833d7328106e7
Felix Angelov <felix@shorebird.dev>
2023-03-22 17:45:24 -0500

    fixes

--- packages/shorebird_cli/test/src/command_runner_test.dart
+++ packages/shorebird_cli/test/src/command_runner_test.dart
@@ -6,6 +6,7 @@ import 'package:mason_logger/mason_logger.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:pub_updater/pub_updater.dart';
 import 'package:shorebird_cli/src/command_runner.dart';
+import 'package:shorebird_cli/src/flutter_engine_revision.dart';
 import 'package:shorebird_cli/src/version.dart';
 import 'package:test/test.dart';
 
@@ -43,10 +44,10 @@ void main() {
       when(() => processResult.exitCode).thenReturn(ExitCode.success.code);
       when(() => processResult.stdout).thenReturn(
         '''
-Flutter 3.7.7 • channel stable • git@github.com:flutter/flutter.git
-Framework • revision 2ad6cd72c0 (12 days ago) • 2023-03-08 09:41:59 -0800
-Engine • revision 1837b5be5f
-Tools • Dart 2.19.4 • DevTools 2.20.1
+Flutter 3.7.8 • channel stable • git@github.com:flutter/flutter.git
+Framework • revision 90c64ed42b (30 hours ago) • 2023-03-21 11:27:08 -0500
+Engine • revision $requiredFlutterEngineRevision
+Tools • Dart 2.19.5 • DevTools 2.20.1
 ''',
       );
 
