1ed84ec0896058fbd93625d04a3545387782b1cc
Eric Seidel <eric@shorebird.dev>
2023-03-22 09:52:39 -0700

    Update bootstrap.sh
    Add a comment about paths.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script assumes that you already have $HOME/.pub-cache/bin as well as
+# Flutter's bin (for `dart`) in your PATH.
+# If you don't, add it to your .bashrc or .zshrc file.
+# export PATH=$HOME/.pub-cache/bin:$PATH
+
 # Fetch dart dependencies
 dart pub global activate very_good_cli
 very_good --analytics=false
