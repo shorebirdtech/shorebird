447d0385eea0b18123f41b1c4df9c66cabdb7e1a
Felix Angelov <felix@shorebird.dev>
2023-03-23 16:07:38 -0500

    feat(code_push_protocol): add `Artifact`, `Channel`, and `Release` (#145)

---
+++ packages/shorebird_code_push_protocol/lib/src/models/channel.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: implicit_dynamic_parameter, require_trailing_commas, cast_nullable_to_non_nullable, lines_longer_than_80_chars
+
+part of 'channel.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Channel _$ChannelFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'Channel',
+      json,
+      ($checkedConvert) {
+        final val = Channel(
+          id: $checkedConvert('id', (v) => v as int),
+          appId: $checkedConvert('app_id', (v) => v as String),
+          name: $checkedConvert('name', (v) => v as String),
+        );
+        return val;
+      },
+      fieldKeyMap: const {'appId': 'app_id'},
+    );
+
+Map<String, dynamic> _$ChannelToJson(Channel instance) => <String, dynamic>{
+      'id': instance.id,
+      'app_id': instance.appId,
+      'name': instance.name,
+    };
--- packages/shorebird_code_push_protocol/lib/src/models/models.dart
+++ packages/shorebird_code_push_protocol/lib/src/models/models.dart
@@ -1,5 +1,8 @@
 export 'app.dart';
 export 'app_metadata.dart';
+export 'artifact.dart';
+export 'channel.dart';
 export 'error_response.dart';
 export 'patch.dart';
+export 'release.dart';
 export 'user.dart';
