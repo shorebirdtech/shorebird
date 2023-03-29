0220589c7ae151b429cb6f825a6e36dc1eb5315d
Felix Angelov <felix@shorebird.dev>
2023-03-29 15:30:50 -0500

    feat(shorebird_cli): improve `shorebird apps list` display (#202)

--- packages/shorebird_cli/lib/src/commands/apps/list_apps_command.dart
+++ packages/shorebird_cli/lib/src/commands/apps/list_apps_command.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:barbecue/barbecue.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:shorebird_cli/src/command.dart';
 import 'package:shorebird_cli/src/shorebird_config_mixin.dart';
@@ -40,7 +41,7 @@ class ListAppsCommand extends ShorebirdCommand with ShorebirdConfigMixin {
       hostedUri: hostedUri,
     );
 
-    late final List<AppMetadata> apps;
+    final List<AppMetadata> apps;
     try {
       apps = await client.getApps();
     } catch (error) {
@@ -48,25 +49,56 @@ class ListAppsCommand extends ShorebirdCommand with ShorebirdConfigMixin {
       return ExitCode.software.code;
     }
 
+    logger.info('📱 Apps');
+
     if (apps.isEmpty) {
       logger.info('(empty)');
       return ExitCode.success.code;
     }
 
-    for (final app in apps) {
-      logger.info(app.prettyPrint());
-    }
+    logger.info(apps.prettyPrint());
 
     return ExitCode.success.code;
   }
 }
 
-extension on AppMetadata {
+extension on List<AppMetadata> {
   String prettyPrint() {
-    final latestReleasePart =
-        latestReleaseVersion != null ? 'v$latestReleaseVersion' : '(empty)';
-    final latestPatchPart =
-        latestPatchNumber != null ? ' (patch #$latestPatchNumber)' : '';
-    return '$displayName: $latestReleasePart$latestPatchPart ($appId)';
+    const cellStyle = CellStyle(
+      paddingLeft: 1,
+      paddingRight: 1,
+      borderBottom: true,
+      borderTop: true,
+      borderLeft: true,
+      borderRight: true,
+    );
+    return Table(
+      cellStyle: cellStyle,
+      header: const TableSection(
+        rows: [
+          Row(
+            cells: [
+              Cell('Name'),
+              Cell('ID'),
+              Cell('Release'),
+              Cell('Patch'),
+            ],
+          )
+        ],
+      ),
+      body: TableSection(
+        rows: [
+          for (final app in this)
+            Row(
+              cells: [
+                Cell(app.displayName),
+                Cell(app.appId),
+                Cell(app.latestReleaseVersion ?? '--'),
+                Cell(app.latestPatchNumber?.toString() ?? '--'),
+              ],
+            ),
+        ],
+      ),
+    ).render();
   }
 }
