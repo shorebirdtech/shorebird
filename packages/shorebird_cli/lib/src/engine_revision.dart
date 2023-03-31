e546f59345ffddaf760ef05bcb14f54557e38f9d
Felix Angelov <felix@shorebird.dev>
2023-03-31 13:11:56 -0500

    chore(shorebird_cli): bump engine revision

--- packages/shorebird_cli/lib/src/engine_revision.dart
+++ packages/shorebird_cli/lib/src/engine_revision.dart
@@ -3,4 +3,4 @@
 const requiredFlutterEngineRevision = '9aa7816315';
 
 /// The hash of the Shorebird engine used by this version of Shorebird CLI.
-const shorebirdEngineRevision = 'b6e8c4ecd9';
+const shorebirdEngineRevision = 'ff32625d5b';
