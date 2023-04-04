980224ce92c82740ff419031edb674fab9edda65
Felix Angelov <felix@shorebird.dev>
2023-04-04 10:52:49 -0500

    more test fixes

--- packages/shorebird_cli/test/src/auth/auth_test.dart
+++ packages/shorebird_cli/test/src/auth/auth_test.dart
@@ -13,6 +13,12 @@ void main() {
     const idToken =
         '''eyJhbGciOiJSUzI1NiIsImN0eSI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZW1haWwuY29tIn0.pD47BhF3MBLyIpfsgWCzP9twzC1HJxGukpcR36DqT6yfiOMHTLcjDbCjRLAnklWEHiT0BQTKTfhs8IousU90Fm5bVKObudfKu8pP5iZZ6Ls4ohDjTrXky9j3eZpZjwv8CnttBVgRfMJG-7YASTFRYFcOLUpnb4Zm5R6QdoCDUYg''';
     const email = 'test@email.com';
+    final credentials = AccessCredentials(
+      AccessToken('Bearer', 'accessToken', DateTime.now().toUtc()),
+      '',
+      [],
+      idToken: idToken,
+    );
 
     late http.Client httpClient;
     late AccessCredentials accessCredentials;
@@ -24,7 +30,7 @@ void main() {
       auth = Auth(
         httpClient: httpClient,
         obtainAccessCredentials: (clientId, scopes, client, userPrompt) async {
-          return accessCredentials;
+          return credentials;
         },
       )..logout();
     });
@@ -63,6 +69,11 @@ void main() {
 
       test('should not set the user when token is null', () async {
         when(() => accessCredentials.idToken).thenReturn(null);
+        auth = Auth(
+          httpClient: httpClient,
+          obtainAccessCredentials:
+              (clientId, scopes, client, userPrompt) async => accessCredentials,
+        );
         await expectLater(auth.login((_) {}), throwsA(isException));
         expect(auth.user, isNull);
         expect(auth.isAuthenticated, isFalse);
@@ -70,6 +81,11 @@ void main() {
 
       test('should not set the user when token is empty', () async {
         when(() => accessCredentials.idToken).thenReturn('');
+        auth = Auth(
+          httpClient: httpClient,
+          obtainAccessCredentials:
+              (clientId, scopes, client, userPrompt) async => accessCredentials,
+        );
         await expectLater(auth.login((_) {}), throwsA(isException));
         expect(auth.user, isNull);
         expect(auth.isAuthenticated, isFalse);
@@ -78,6 +94,11 @@ void main() {
       test('should not set the user when token claims are malformed', () async {
         when(() => accessCredentials.idToken).thenReturn(
           '''eyJhbGciOiJSUzI1NiIsImN0eSI6IkpXVCJ9.eyJmb28iOiJiYXIifQ.LaR0JfOiDrS1AuABC38kzxpSjRLJ_OtfOkZ8hL6I1GPya-cJYwsmqhi5eMBwEbpYHcJhguG5l56XM6dW8xjdK7JbUN6_53gHBosSnL-Ccf29oW71Ado9sxO17YFQyihyMofJ_v78BPVy2H5O10hNjRn_M0JnnAe0Fvd2VrInlIE''',
+        );
+        auth = Auth(
+          httpClient: httpClient,
+          obtainAccessCredentials:
+              (clientId, scopes, client, userPrompt) async => accessCredentials,
         );
         await expectLater(auth.login((_) {}), throwsA(isException));
         expect(auth.user, isNull);
