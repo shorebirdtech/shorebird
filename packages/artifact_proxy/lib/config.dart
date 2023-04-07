224bae561a2c05763ec05532955f75121377ecf6
Eric Seidel <eric@shorebird.dev>
2023-04-07 10:18:42 -0700

    wip

--- packages/artifact_proxy/lib/config.dart
+++ packages/artifact_proxy/lib/config.dart
@@ -1,17 +1,17 @@
 import 'package:artifact_proxy/artifact_proxy.dart';
 
 /// The proxy configuration used by the server.
-const config = ProxyConfig(
+ProxyConfig config = ProxyConfig(
   engineMappings: {
     ...flutter_3_7_10,
     ...flutter_3_7_8,
   },
 );
 
-/// Flutter 3.7.10
-const flutter_3_7_10 = {
-  '978a56f2d97f9ce24a2b6bc22c9bbceaaba0343c': EngineMapping(
-    flutterEngineRevision: 'ec975089acb540fc60752606a3d3ba809dd1528b',
+// On the assumption artifact layouts don't change very often for Flutter.
+EngineMapping _flutter3_7(String flutterEngineRevision) {
+  return EngineMapping(
+    flutterEngineRevision: flutterEngineRevision,
     shorebirdStorageBucket: 'download.shorebird.dev',
     shorebirdArtifactOverrides: {
       // artifacts.zip
@@ -35,35 +35,19 @@ const flutter_3_7_10 = {
       r'download.flutter.io/io/flutter/x86_64_release/1.0.0-$engine/x86_64_release-1.0.0-$engine.pom',
       r'download.flutter.io/io/flutter/x86_64_release/1.0.0-$engine/x86_64_release-1.0.0-$engine.jar',
     },
-  ),
+  );
+}
+
+/// Flutter 3.7.10
+Map<String, EngineMapping> flutter_3_7_10 = {
+  '978a56f2d97f9ce24a2b6bc22c9bbceaaba0343c':
+      _flutter3_7('ec975089acb540fc60752606a3d3ba809dd1528b'),
+  '7aa5c44764e10722d188ece75819f7d10f5269a3':
+      _flutter3_7('ec975089acb540fc60752606a3d3ba809dd1528b'),
 };
 
 /// Flutter 3.7.8
-const flutter_3_7_8 = {
-  '79f4c5321a581f580a9bda01ec372cbf4a53aa53': EngineMapping(
-    flutterEngineRevision: '9aa7816315095c86410527932918c718cb35e7d6',
-    shorebirdStorageBucket: 'download.shorebird.dev',
-    shorebirdArtifactOverrides: {
-      // artifacts.zip
-      r'flutter_infra_release/flutter/$engine/android-arm-64-release/artifacts.zip',
-      r'flutter_infra_release/flutter/$engine/android-arm-release/artifacts.zip',
-      r'flutter_infra_release/flutter/$engine/android-x64-release/artifacts.zip',
-
-      // embedding release
-      r'download.flutter.io/io/flutter/flutter_embedding_release/1.0.0-$engine/flutter_embedding_release-1.0.0-$engine.pom',
-      r'download.flutter.io/io/flutter/flutter_embedding_release/1.0.0-$engine/flutter_embedding_release-1.0.0-$engine.jar',
-
-      // arm64_v8a release
-      r'download.flutter.io/io/flutter/arm64_v8a_release/1.0.0-$engine/arm64_v8a_release-1.0.0-$engine.pom',
-      r'download.flutter.io/io/flutter/arm64_v8a_release/1.0.0-$engine/arm64_v8a_release-1.0.0-$engine.jar',
-
-      // armeabi_v7a release
-      r'download.flutter.io/io/flutter/armeabi_v7a_release/1.0.0-$engine/armeabi_v7a_release-1.0.0-$engine.pom',
-      r'download.flutter.io/io/flutter/armeabi_v7a_release/1.0.0-$engine/armeabi_v7a_release-1.0.0-$engine.jar',
-
-      // x86_64 release
-      r'download.flutter.io/io/flutter/x86_64_release/1.0.0-$engine/x86_64_release-1.0.0-$engine.pom',
-      r'download.flutter.io/io/flutter/x86_64_release/1.0.0-$engine/x86_64_release-1.0.0-$engine.jar',
-    },
-  ),
+Map<String, EngineMapping> flutter_3_7_8 = {
+  '79f4c5321a581f580a9bda01ec372cbf4a53aa53':
+      _flutter3_7('9aa7816315095c86410527932918c718cb35e7d6'),
 };
