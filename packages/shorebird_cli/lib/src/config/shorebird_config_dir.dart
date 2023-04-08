2431942afa26a0e4b43683859078aa6e767346bf
erikas-taroza <erikastaroza@gmail.com>
2023-04-07 21:15:30 -0700

    Fix `shorebirdConfigDir` not pointing to `testApplicationConfigHome` in tests

--- packages/shorebird_cli/lib/src/config/shorebird_config_dir.dart
+++ packages/shorebird_cli/lib/src/config/shorebird_config_dir.dart
@@ -2,10 +2,10 @@ import 'package:cli_util/cli_util.dart';
 import 'package:meta/meta.dart';
 import 'package:shorebird_cli/src/command_runner.dart';
 
-final String shorebirdConfigDir = () {
+String get shorebirdConfigDir {
   final configHome = testApplicationConfigHome ?? applicationConfigHome;
   return configHome(executableName);
-}();
+}
 
 /// Test applicationConfigHome which should
 /// only be used for testing purposes.
