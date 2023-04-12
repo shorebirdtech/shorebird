28a925d58d0a0d5e410ec17edc660bfbb689e352
Bryan Oltman <bryanoltman@gmail.com>
2023-04-12 11:32:14 -0400

    feat(shorebird_code_push_protocol): Add email field to User (#276)

--- packages/shorebird_code_push_protocol/test/src/models/user_test.dart
+++ packages/shorebird_code_push_protocol/test/src/models/user_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 void main() {
   group('User', () {
     test('can be (de)serialized', () {
-      const user = User(id: 1);
+      const user = User(id: 1, email: 'test@shorebird.dev');
       expect(
         User.fromJson(user.toJson()).toJson(),
         equals(user.toJson()),
