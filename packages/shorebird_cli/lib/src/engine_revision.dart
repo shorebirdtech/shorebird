dc0db5b6aff9f50b70dbe41389897638cafd508f
Felix Angelov <felix@shorebird.dev>
2023-04-17 17:21:48 -0500

    chore(shorebird_cli): v0.0.6 (#303)

--- packages/shorebird_cli/lib/src/engine_revision.dart
+++ packages/shorebird_cli/lib/src/engine_revision.dart
@@ -1,2 +1,2 @@
 /// The hash of the Shorebird engine used by this version of Shorebird CLI.
-const shorebirdEngineRevision = 'e6a2a5a43973430d9f038cd81cb1779b6b404909';
+const shorebirdEngineRevision = '8b89f8bd9fc6982aa9c4557fd0e5e89db1ff9986';
