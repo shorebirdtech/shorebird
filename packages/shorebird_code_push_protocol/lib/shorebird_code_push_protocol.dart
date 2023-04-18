3f9d89e289604fcba55d1eb40bed36a3600016c4
Bryan Oltman <bryan@shorebird.dev>
2023-04-18 16:19:08 -0400

    Export converters

--- packages/shorebird_code_push_protocol/lib/shorebird_code_push_protocol.dart
+++ packages/shorebird_code_push_protocol/lib/shorebird_code_push_protocol.dart
@@ -1,6 +1,7 @@
 /// The Shorebird CodePush Protocol
 library shorebird_code_push_protocol;
 
+export 'src/converters/converters.dart';
 export 'src/messages/messages.dart';
 export 'src/models/models.dart';
 
