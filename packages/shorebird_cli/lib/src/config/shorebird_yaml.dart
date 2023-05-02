aca763b81b150fe9fbd1c7b08514d580ee99e51f
Felix Angelov <felix@shorebird.dev>
2023-05-02 14:30:10 -0500

    add docs

--- packages/shorebird_cli/lib/src/config/shorebird_yaml.dart
+++ packages/shorebird_cli/lib/src/config/shorebird_yaml.dart
@@ -2,12 +2,16 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'shorebird_yaml.g.dart';
 
+/// {@template shorebird_yaml}
+/// A Shorebird configuration file which contains metadata about the app.
+/// {@endtemplate}
 @JsonSerializable(
   anyMap: true,
   disallowUnrecognizedKeys: true,
   createToJson: false,
 )
 class ShorebirdYaml {
+  /// {@macro shorebird_yaml}
   const ShorebirdYaml({required this.appId, this.baseUrl});
 
   factory ShorebirdYaml.fromJson(Map<dynamic, dynamic> json) =>
@@ -18,6 +22,10 @@ class ShorebirdYaml {
   final String? baseUrl;
 }
 
+/// {@template app_id}
+/// The unique identifier for the app. Can be a single string or a map of
+/// flavor names to ids for multi-flavor apps.
+/// {@endtemplate}
 class AppId {
   const AppId({this.value, this.values});
 
