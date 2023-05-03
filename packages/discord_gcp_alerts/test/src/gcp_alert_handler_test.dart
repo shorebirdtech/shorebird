c164cde617ec250c6b7642a38348adcb74b8c8a8
Felix Angelov <felix@shorebird.dev>
2023-05-03 10:09:40 -0500

    feat(discord_gcp_alerts): include policy name in alert title

--- packages/discord_gcp_alerts/test/src/gcp_alert_handler_test.dart
+++ packages/discord_gcp_alerts/test/src/gcp_alert_handler_test.dart
@@ -94,7 +94,7 @@ void main() {
           body: json.encode({
             'embeds': [
               {
-                'title': 'GCP Alert',
+                'title': '[GCP Alert] projects/12345/alertPolicies/12345',
                 'description': 'Test Incident',
                 'url': 'http://www.example.com',
                 'color': 16711680,
