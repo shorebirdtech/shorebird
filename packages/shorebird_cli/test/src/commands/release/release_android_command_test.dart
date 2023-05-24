146f84b9bceafe19ac5a43de515aad9365329c3e
Felix Angelov <felix@shorebird.dev>
2023-05-24 14:16:51 -0700

    refactor(shorebird_cli): refactor `shorebird release` in favor of `shorebird release android` (#541)

--- packages/shorebird_cli/test/src/commands/release/release_android_command_test.dart
+++ packages/shorebird_cli/test/src/commands/release/release_android_command_test.dart
@@ -40,7 +40,7 @@ class _MockShorebirdFlutterValidator extends Mock
 class _MockShorebirdProcess extends Mock implements ShorebirdProcess {}
 
 void main() {
-  group(ReleaseCommand, () {
+  group(ReleaseAndroidCommand, () {
     const appId = 'test-app-id';
     const flutterRevision = '83305b5088e6fe327fb3334a73ff190828d85713';
     const versionName = '1.2.3';
@@ -90,7 +90,7 @@ flutter:
     late ShorebirdProcessResult releaseVersionNameProcessResult;
     late ShorebirdProcessResult releaseVersionCodeProcessResult;
     late CodePushClient codePushClient;
-    late ReleaseCommand command;
+    late ReleaseAndroidCommand command;
     late Uri? capturedHostedUri;
     late ShorebirdFlutterValidator flutterValidator;
     late ShorebirdProcess shorebirdProcess;
@@ -147,7 +147,7 @@ flutter:
       codePushClient = _MockCodePushClient();
       flutterValidator = _MockShorebirdFlutterValidator();
       shorebirdProcess = _MockShorebirdProcess();
-      command = ReleaseCommand(
+      command = ReleaseAndroidCommand(
         auth: auth,
         buildCodePushClient: ({
           required http.Client httpClient,
@@ -265,20 +265,8 @@ flutter:
       when(() => flutterValidator.validate(any())).thenAnswer((_) async => []);
     });
 
-    test('logs deprecation warning', () async {
-      final tempDir = Directory.systemTemp.createTempSync();
-      final exitCode = await IOOverrides.runZoned(
-        command.run,
-        getCurrentDirectory: () => tempDir,
-      );
-      verify(
-        () => logger.warn(
-          '''
-"shorebird release" is deprecated.
-Please use "shorebird release android" instead.''',
-        ),
-      ).called(1);
-      expect(exitCode, ExitCode.config.code);
+    test('has a description', () {
+      expect(command.description, isNotEmpty);
     });
 
     test('throws config error when shorebird is not initialized', () async {
