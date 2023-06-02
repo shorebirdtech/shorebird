0a8e1d88583b6a9f415abe2422af72a9b57ce36a
Felix Angelov <felix@shorebird.dev>
2023-06-02 13:10:02 -0500

    tweak

--- packages/shorebird_code_push_protocol/lib/src/messages/create_patch/create_patch_response.dart
+++ packages/shorebird_code_push_protocol/lib/src/messages/create_patch/create_patch_response.dart
@@ -3,7 +3,7 @@ import 'package:json_annotation/json_annotation.dart';
 part 'create_patch_response.g.dart';
 
 /// {@template create_patch_response}
-/// The response body for `POST /api/v1/patches`
+/// The response body for POST /api/v1/patches
 /// {@endtemplate}
 @JsonSerializable()
 class CreatePatchResponse {
