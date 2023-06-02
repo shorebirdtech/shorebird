c1f38186e6733b1a0ab91dd4043bb52bcef4bc53
Felix Angelov <felix@shorebird.dev>
2023-06-02 13:19:27 -0500

    tweak

--- packages/shorebird_code_push_protocol/lib/src/messages/messages.dart
+++ packages/shorebird_code_push_protocol/lib/src/messages/messages.dart
@@ -2,7 +2,7 @@ export 'cancel_subscription/cancel_subscription_response.dart';
 export 'check_for_patches/check_for_patches.dart';
 export 'create_app/create_app.dart';
 export 'create_app_collaborator/create_app_collaborator.dart';
-export 'create_artifact/create_artifact.dart';
+export 'create_patch_artifact/create_patch_artifact.dart';
 export 'create_channel/create_channel.dart';
 export 'create_patch/create_patch.dart';
 export 'create_payment_link/create_payment_link.dart';
