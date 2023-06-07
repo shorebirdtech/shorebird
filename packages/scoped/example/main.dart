7ecfac1b5ca1599893ff58cbec1d92da92f97af5
Felix Angelov <felix@shorebird.dev>
2023-06-07 10:13:12 -0500

    feat: add `package:scoped`

---
+++ packages/scoped/example/main.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: avoid_print
+import 'package:scoped/scoped.dart';
+
+final value = create(() => 42);
+
+void main() {
+  runScoped(scopeA, values: {value});
+}
+
+void scopeA() {
+  print(read(value)); // 42
+  runScoped(scopeB, values: {value.overrideWith(() => 0)});
+}
+
+void scopeB() {
+  print(read(value)); // 0
+}
