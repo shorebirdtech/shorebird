95cbde5894ae7261bcd101fb7a5aaaf958acdc04
Eric Seidel <eric@shorebird.dev>
2023-06-08 17:46:47 -0700

    chore: fix wrapping of comment, mostly to trigger coverage

--- packages/shorebird_cli/lib/src/command.dart
+++ packages/shorebird_cli/lib/src/command.dart
@@ -117,7 +117,8 @@ abstract class ShorebirdCommand extends Command<int> {
   }
 
   Uri? get hostedUri {
-    // TODO(bryanoltman): maybe look for an environment variable instead of reading shorebird.yaml?
+    // TODO(bryanoltman): maybe look for an environment variable instead of
+    // reading shorebird.yaml?
     try {
       final baseUrl = getShorebirdYaml()?.baseUrl;
       return baseUrl == null ? null : Uri.tryParse(baseUrl);
