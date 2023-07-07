c8e135343838556da89d087f18f202564bd7e4d4
Felix Angelov <felix@shorebird.dev>
2023-07-07 13:57:25 -0500

    feat(shorebird_cli): improve missing options usage error

--- packages/shorebird_cli/lib/src/command_runner.dart
+++ packages/shorebird_cli/lib/src/command_runner.dart
@@ -110,8 +110,20 @@ class ShorebirdCliCommandRunner extends CompletionCommandRunner<int> {
     } on UsageException catch (e) {
       // On usage errors, show the commands usage message and
       // exit with an error code
+
+      logger.err(e.message);
+      if (e.message.contains('Could not find an option named')) {
+        logger.err(
+          '''
+To proxy an option to the flutter command, use the -- --<option> syntax.
+
+Example:
+
+${lightCyan.wrap('shorebird run -- --no-pub lib/main.dart')}''',
+        );
+      }
+
       logger
-        ..err(e.message)
         ..info('')
         ..info(e.usage);
       return ExitCode.usage.code;
