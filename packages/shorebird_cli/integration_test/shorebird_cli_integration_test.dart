79efd2803eb565db1cfa4bf0e614d598ec2d4e88
bryanoltman <bryan@shorebird.dev>
2023-07-10 14:43:34 -0400

    chore(shorebird_cli): add integration test for init, release, and patch

--- packages/shorebird_cli/integration_test/shorebird_cli_integration_test.dart
+++ packages/shorebird_cli/integration_test/shorebird_cli_integration_test.dart
@@ -1,19 +1,100 @@
 import 'dart:io';
 
+import 'package:mason_logger/mason_logger.dart';
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
 void main() {
-  test('--version', () async {
-    final result = await Process.run(
+  final logger = Logger();
+
+  ProcessResult runShorebirdCommand(
+    String command, {
+    required String workingDirectory,
+  }) {
+    logger.info('running shorebird $command in $workingDirectory');
+    return Process.runSync(
       'shorebird',
-      ['--version'],
+      command.split(' '),
       runInShell: true,
+      workingDirectory: workingDirectory,
     );
+  }
+
+  test('--version', () {
+    final result = runShorebirdCommand('--version', workingDirectory: '.');
     expect(result.stderr, isEmpty);
     expect(
       result.stdout,
       stringContainsInOrder(['Shorebird Engine', 'revision']),
     );
     expect(result.exitCode, 0);
+  });
+
+  test('create an app with a release and patch', () {
+    final authToken = Platform.environment['SHOREBIRD_TOKEN'];
+    if (authToken == null || authToken.isEmpty) {
+      throw Exception('SHOREBIRD_TOKEN environment variable is not set.');
+    }
+
+    final testAppName =
+        'test_app_${DateTime.now().millisecondsSinceEpoch ~/ 1000}';
+    final tempDir = Directory.systemTemp.createTempSync();
+    final subDirWithSpace = Directory(p.join(tempDir.path, 'flutter directory'))
+      ..createSync();
+    var cwd = subDirWithSpace.path;
+
+    // Create the default flutter counter app
+    logger.info('running `flutter create $testAppName` in $cwd');
+    final createAppResult = Process.runSync(
+      'flutter',
+      ['create', testAppName],
+      runInShell: true,
+      workingDirectory: cwd,
+    );
+    expect(createAppResult.exitCode, 0);
+    expect(createAppResult.stderr, isEmpty);
+
+    cwd = p.join(cwd, testAppName);
+
+    // Initialize Shorebird
+    final initShorebirdResult = runShorebirdCommand(
+      'init',
+      workingDirectory: cwd,
+    );
+    expect(initShorebirdResult.exitCode, 0);
+    expect(initShorebirdResult.stderr, isEmpty);
+
+    // Run the doctor command. This should yield a warning about the
+    // AndroidManifest.xml not containing the internet permission and suggest
+    // that the user run `shorebird doctor --fix`.
+    final shorebirdDoctorResult = runShorebirdCommand(
+      'doctor',
+      workingDirectory: cwd,
+    );
+    expect(shorebirdDoctorResult.exitCode, 0);
+    expect(shorebirdDoctorResult.stdout, contains('shorebird doctor --fix'));
+
+    // Run the suggested `doctor --fix` command.
+    final shorebirdDoctorFixResult = runShorebirdCommand(
+      'doctor --fix',
+      workingDirectory: cwd,
+    );
+    expect(shorebirdDoctorFixResult.exitCode, 0);
+
+    // Create an Android release.
+    final shorebirdReleaseResult = runShorebirdCommand(
+      'release android --force',
+      workingDirectory: cwd,
+    );
+    expect(shorebirdReleaseResult.exitCode, 0);
+    expect(shorebirdReleaseResult.stdout, contains('Published Release!'));
+
+    // Create an Android patch.
+    final shorebirdPatchResult = runShorebirdCommand(
+      'patch android --force',
+      workingDirectory: cwd,
+    );
+    expect(shorebirdPatchResult.exitCode, 0);
+    expect(shorebirdPatchResult.stdout, contains('Published Patch!'));
   });
 }
