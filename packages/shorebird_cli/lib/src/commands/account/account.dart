7f8477585ae2774467ce4f622cef4988ac7bba94
Felix Angelov <felix@shorebird.dev>
2023-07-21 10:40:48 -0500

    chore(shorebird_cli): remove `shorebird account create` and `shorebird account usage`

--- packages/shorebird_cli/lib/src/commands/account/account.dart
+++ packages/shorebird_cli/lib/src/commands/account/account.dart
@@ -1,5 +1,3 @@
 export 'account_command.dart';
-export 'account_usage_command.dart';
-export 'create_account_command.dart';
 export 'downgrade_account_command.dart';
 export 'upgrade_account_command.dart';
