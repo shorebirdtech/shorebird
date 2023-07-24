b3d34277a47688fa57c26f2b5c8161d7b4d0c9ea
Bryan Oltman <bryanoltman@gmail.com>
2023-07-24 11:39:40 -0400

    fix(shorebird_cli): run doctor --fix on init (#895)

--- packages/shorebird_cli/test/src/commands/doctor_command_test.dart
+++ packages/shorebird_cli/test/src/commands/doctor_command_test.dart
@@ -1,51 +1,30 @@
-import 'dart:io';
-
 import 'package:args/args.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:path/path.dart' as p;
 import 'package:scoped/scoped.dart';
 import 'package:shorebird_cli/src/commands/commands.dart';
 import 'package:shorebird_cli/src/doctor.dart';
 import 'package:shorebird_cli/src/logger.dart';
-import 'package:shorebird_cli/src/process.dart';
 import 'package:shorebird_cli/src/shorebird_environment.dart';
 import 'package:shorebird_cli/src/validators/validators.dart';
+import 'package:shorebird_cli/src/version.dart';
 import 'package:test/test.dart';
 
 class _MockArgResults extends Mock implements ArgResults {}
 
-class _MockShorebirdVersionValidator extends Mock
-    implements ShorebirdVersionValidator {}
-
-class _MockAndroidInternetPermissionValidator extends Mock
-    implements AndroidInternetPermissionValidator {}
-
 class _MockDoctor extends Mock implements Doctor {}
-
-class _MockShorebirdFlutterValidator extends Mock
-    implements ShorebirdFlutterValidator {}
 
 class _MockLogger extends Mock implements Logger {}
 
-class _MockProgress extends Mock implements Progress {}
-
-class _MockShorebirdProcess extends Mock implements ShorebirdProcess {}
+class _MockValidator extends Mock implements Validator {}
 
 void main() {
   group('doctor', () {
-    const androidValidatorDescription = 'Android';
-    const appId = 'test-app-id';
-
     late ArgResults argResults;
     late Doctor doctor;
-    late Logger logger;
-    late Progress progress;
     late DoctorCommand command;
-    late AndroidInternetPermissionValidator androidInternetPermissionValidator;
-    late ShorebirdVersionValidator shorebirdVersionValidator;
-    late ShorebirdFlutterValidator shorebirdFlutterValidator;
-    late ShorebirdProcess shorebirdProcess;
+    late Logger logger;
+    late Validator validator;
 
     R runWithOverrides<R>(R Function() body) {
       return runScoped(
@@ -53,317 +32,55 @@ void main() {
         values: {
           doctorRef.overrideWith(() => doctor),
           loggerRef.overrideWith(() => logger),
-          processRef.overrideWith(() => shorebirdProcess),
         },
       );
-    }
-
-    Directory setUpTempDir() {
-      final tempDir = Directory.systemTemp.createTempSync();
-      File(
-        p.join(tempDir.path, 'shorebird.yaml'),
-      ).writeAsStringSync('app_id: $appId');
-      return tempDir;
     }
 
     setUp(() {
       argResults = _MockArgResults();
       doctor = _MockDoctor();
       logger = _MockLogger();
-      progress = _MockProgress();
+      validator = _MockValidator();
 
       ShorebirdEnvironment.shorebirdEngineRevision = 'test-revision';
 
-      when(() => argResults['fix']).thenReturn(false);
-
-      when(() => logger.progress(any())).thenReturn(progress);
-      when(() => logger.info(any())).thenReturn(null);
-
-      androidInternetPermissionValidator =
-          _MockAndroidInternetPermissionValidator();
-      shorebirdVersionValidator = _MockShorebirdVersionValidator();
-      shorebirdFlutterValidator = _MockShorebirdFlutterValidator();
-      shorebirdProcess = _MockShorebirdProcess();
-      registerFallbackValue(shorebirdProcess);
-
-      when(() => androidInternetPermissionValidator.description)
-          .thenReturn(androidValidatorDescription);
-      when(() => androidInternetPermissionValidator.scope)
-          .thenReturn(ValidatorScope.project);
-      when(androidInternetPermissionValidator.validate)
-          .thenAnswer((_) async => []);
-
-      when(() => shorebirdVersionValidator.description)
-          .thenReturn('Shorebird Version');
-      when(() => shorebirdVersionValidator.scope)
-          .thenReturn(ValidatorScope.installation);
-      when(shorebirdVersionValidator.validate).thenAnswer((_) async => []);
-
-      when(() => shorebirdFlutterValidator.description)
-          .thenReturn('Shorebird Flutter');
-      when(() => shorebirdFlutterValidator.scope)
-          .thenReturn(ValidatorScope.installation);
-      when(shorebirdFlutterValidator.validate).thenAnswer((_) async => []);
-
-      when(() => doctor.allValidators).thenReturn([
-        androidInternetPermissionValidator,
-        shorebirdVersionValidator,
-        shorebirdFlutterValidator,
-      ]);
+      when(() => doctor.allValidators).thenReturn([validator]);
+      when(
+        () => doctor.runValidators(any(), applyFixes: any(named: 'applyFixes')),
+      ).thenAnswer((_) async => {});
 
       command = runWithOverrides(DoctorCommand.new)
         ..testArgResults = argResults;
     });
 
-    test('prints "no issues" when everything is OK', () async {
-      final tempDir = setUpTempDir();
+    test('prints shorebird version and engine revision', () async {
+      await runWithOverrides(command.run);
 
-      await IOOverrides.runZoned(
-        () => runWithOverrides(command.run),
-        getCurrentDirectory: () => tempDir,
-      );
-
-      for (final validator in doctor.allValidators) {
-        verify(validator.validate).called(1);
-      }
       verify(
-        () => logger.info(any(that: contains('No issues detected'))),
+        () => logger.info('''
+
+Shorebird v$packageVersion
+Shorebird Engine • revision ${ShorebirdEnvironment.shorebirdEngineRevision}
+'''),
       ).called(1);
     });
 
-    test('prints messages when warnings or errors found', () async {
-      when(androidInternetPermissionValidator.validate).thenAnswer(
-        (_) async => [
-          const ValidationIssue(
-            severity: ValidationIssueSeverity.warning,
-            message: 'oh no!',
-          ),
-          const ValidationIssue(
-            severity: ValidationIssueSeverity.error,
-            message: 'OH NO!',
-          ),
-        ],
-      );
-
-      final tempDir = setUpTempDir();
-
-      await IOOverrides.runZoned(
-        () => runWithOverrides(command.run),
-        getCurrentDirectory: () => tempDir,
-      );
-
-      for (final validator in doctor.allValidators) {
-        verify(validator.validate).called(1);
-      }
-
-      verify(
-        () => logger.info(any(that: stringContainsInOrder(['[!]', 'oh no!']))),
-      ).called(1);
-
-      verify(
-        () => logger.info(any(that: stringContainsInOrder(['[✗]', 'OH NO!']))),
-      ).called(1);
-
-      verify(
-        () => logger.info(any(that: contains('2 issues detected.'))),
-      ).called(1);
-    });
-
-    test(
-        '''does not run project-specific validators if not in a project directory''',
+    test('runs validators without applying fixes if no fix flag exists',
         () async {
-      await IOOverrides.runZoned(
-        () => runWithOverrides(command.run),
-        getCurrentDirectory: () => Directory.systemTemp.createTempSync(),
-      );
+      when(() => argResults['fix']).thenReturn(null);
+      final result = await runWithOverrides(command.run);
 
-      verify(shorebirdFlutterValidator.validate).called(1);
-      verify(shorebirdVersionValidator.validate).called(1);
-      verifyNever(androidInternetPermissionValidator.validate);
+      expect(result, equals(ExitCode.success.code));
+      verify(() => doctor.runValidators([validator])).called(1);
     });
 
-    group('fix', () {
-      test('tells the user we can fix issues if we can', () async {
-        when(androidInternetPermissionValidator.validate).thenAnswer(
-          (_) async => [
-            ValidationIssue(
-              severity: ValidationIssueSeverity.warning,
-              message: 'oh no!',
-              fix: () async {},
-            ),
-          ],
-        );
-        final tempDir = setUpTempDir();
+    test('runs validators and applies fixes fix flag is true', () async {
+      when(() => argResults['fix']).thenReturn(true);
+      final result = await runWithOverrides(command.run);
 
-        await IOOverrides.runZoned(
-          () => runWithOverrides(command.run),
-          getCurrentDirectory: () => tempDir,
-        );
-
-        verify(
-          () => logger.info(
-            any(
-              that: stringContainsInOrder([
-                '1 issue can be fixed automatically',
-                'shorebird doctor --fix',
-              ]),
-            ),
-          ),
-        ).called(1);
-      });
-
-      test('does not tell the user we can fix issues if we cannot', () async {
-        when(androidInternetPermissionValidator.validate).thenAnswer(
-          (_) async => [
-            const ValidationIssue(
-              severity: ValidationIssueSeverity.warning,
-              message: 'oh no!',
-            ),
-          ],
-        );
-        final tempDir = setUpTempDir();
-
-        await IOOverrides.runZoned(
-          () => runWithOverrides(command.run),
-          getCurrentDirectory: () => tempDir,
-        );
-
-        verifyNever(
-          () => logger.info(
-            any(
-              that: stringContainsInOrder([
-                'We can fix some of these issues',
-                'shorebird doctor --fix',
-              ]),
-            ),
-          ),
-        );
-      });
-
-      test('does not fix issues if --fix flag is not provided', () async {
-        when(() => argResults['fix']).thenReturn(false);
-
-        var fixCalled = false;
-        when(androidInternetPermissionValidator.validate).thenAnswer(
-          (_) async => [
-            ValidationIssue(
-              severity: ValidationIssueSeverity.warning,
-              message: 'oh no!',
-              fix: () => fixCalled = true,
-            ),
-          ],
-        );
-
-        final tempDir = setUpTempDir();
-
-        await IOOverrides.runZoned(
-          () => runWithOverrides(command.run),
-          getCurrentDirectory: () => tempDir,
-        );
-
-        expect(fixCalled, isFalse);
-        verifyNever(() => progress.update('Fixing'));
-        verify(() => progress.fail(androidValidatorDescription)).called(1);
-        verify(androidInternetPermissionValidator.validate).called(1);
-      });
-
-      test('fixes issues if the --fix flag is provided', () async {
-        when(() => argResults['fix']).thenReturn(true);
-
-        var fixCalled = false;
-        final issues = [
-          ValidationIssue(
-            severity: ValidationIssueSeverity.warning,
-            message: 'oh no!',
-            fix: () => fixCalled = true,
-          ),
-        ];
-        when(androidInternetPermissionValidator.validate).thenAnswer(
-          (_) async {
-            if (issues.isEmpty) return [];
-            return [issues.removeLast()];
-          },
-        );
-        final tempDir = setUpTempDir();
-
-        await IOOverrides.runZoned(
-          () => runWithOverrides(command.run),
-          getCurrentDirectory: () => tempDir,
-        );
-
-        expect(fixCalled, isTrue);
-        verify(() => progress.update('Fixing')).called(1);
-        verify(
-          () => progress.complete(any(that: contains('1 fix applied'))),
-        ).called(1);
-        verify(androidInternetPermissionValidator.validate).called(2);
-      });
-
-      test('does not print "fixed" if fix fails', () async {
-        when(() => argResults['fix']).thenReturn(true);
-
-        var fixCalled = false;
-        when(androidInternetPermissionValidator.validate).thenAnswer(
-          (_) async => [
-            ValidationIssue(
-              severity: ValidationIssueSeverity.warning,
-              message: 'oh no!',
-              fix: () => fixCalled = true,
-            ),
-          ],
-        );
-
-        final tempDir = setUpTempDir();
-
-        await IOOverrides.runZoned(
-          () => runWithOverrides(command.run),
-          getCurrentDirectory: () => tempDir,
-        );
-
-        expect(fixCalled, isTrue);
-        verify(() => progress.update('Fixing')).called(1);
-        verify(() => progress.fail(androidValidatorDescription)).called(1);
-        verifyNever(
-          () => progress.complete(any(that: contains('fix applied'))),
-        );
-        verifyNever(
-          () => progress.complete(any(that: contains('fixes applied'))),
-        );
-        verify(androidInternetPermissionValidator.validate).called(2);
-      });
-
-      test('prints error and continues if fix() throws', () async {
-        when(() => argResults['fix']).thenReturn(true);
-        when(androidInternetPermissionValidator.validate).thenAnswer(
-          (_) async => [
-            ValidationIssue(
-              severity: ValidationIssueSeverity.warning,
-              message: 'oh no!',
-              fix: () => throw Exception('oh no!'),
-            ),
-          ],
-        );
-
-        final tempDir = setUpTempDir();
-
-        await IOOverrides.runZoned(
-          () => runWithOverrides(command.run),
-          getCurrentDirectory: () => tempDir,
-        );
-
-        verify(() => progress.update('Fixing')).called(1);
-        verify(androidInternetPermissionValidator.validate).called(2);
-        verify(
-          () => logger.err(
-            any(
-              that: stringContainsInOrder([
-                'An error occurred while attempting to fix',
-                'oh no!',
-              ]),
-            ),
-          ),
-        ).called(1);
-      });
+      expect(result, equals(ExitCode.success.code));
+      verify(() => doctor.runValidators([validator], applyFixes: true))
+          .called(1);
     });
   });
 }
---
+++ packages/shorebird_cli/test/src/doctor_test.dart
@@ -0,0 +1,266 @@
+import 'dart:io';
+
+import 'package:mason_logger/mason_logger.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:path/path.dart' as p;
+import 'package:scoped/scoped.dart';
+import 'package:shorebird_cli/src/doctor.dart';
+import 'package:shorebird_cli/src/logger.dart';
+import 'package:shorebird_cli/src/validators/validators.dart';
+import 'package:test/test.dart';
+
+class _MockLogger extends Mock implements Logger {}
+
+class _MockProgress extends Mock implements Progress {}
+
+class _MockValidator extends Mock implements Validator {}
+
+void main() {
+  group(Doctor, () {
+    const validationWarning = ValidationIssue(
+      severity: ValidationIssueSeverity.warning,
+      message: 'warning',
+    );
+    const validationError = ValidationIssue(
+      severity: ValidationIssueSeverity.error,
+      message: 'error',
+    );
+
+    late Logger logger;
+    late Progress progress;
+    late Validator noIssuesValidator;
+    late Validator warningValidator;
+    late Validator errorValidator;
+    late Doctor doctor;
+
+    R runWithOverrides<R>(R Function() body) {
+      return runScoped(
+        body,
+        values: {
+          loggerRef.overrideWith(() => logger),
+        },
+      );
+    }
+
+    setUp(() {
+      logger = _MockLogger();
+      progress = _MockProgress();
+      noIssuesValidator = _MockValidator();
+      warningValidator = _MockValidator();
+      errorValidator = _MockValidator();
+
+      doctor = Doctor();
+
+      when(() => logger.progress(any())).thenReturn(progress);
+      when(() => logger.info(any())).thenReturn(null);
+
+      when(noIssuesValidator.validate).thenAnswer((_) async => []);
+      when(() => noIssuesValidator.scope)
+          .thenReturn(ValidatorScope.installation);
+      when(() => noIssuesValidator.description)
+          .thenReturn('no issues validator');
+
+      when(warningValidator.validate).thenAnswer(
+        (_) async => [validationWarning],
+      );
+      when(() => warningValidator.scope)
+          .thenReturn(ValidatorScope.installation);
+      when(() => warningValidator.description).thenReturn('warning validator');
+
+      when(errorValidator.validate).thenAnswer((_) async => [validationError]);
+      when(() => errorValidator.scope).thenReturn(ValidatorScope.installation);
+      when(() => errorValidator.description).thenReturn('error validator');
+    });
+
+    group('validate', () {
+      test('prints messages when warnings and errors found', () async {
+        final validators = [
+          warningValidator,
+          errorValidator,
+        ];
+        await runWithOverrides(() => doctor.runValidators(validators));
+
+        for (final validator in validators) {
+          verify(validator.validate).called(1);
+        }
+
+        verify(
+          () =>
+              logger.info(any(that: stringContainsInOrder(['[!]', 'warning']))),
+        ).called(1);
+
+        verify(
+          () => logger.info(any(that: stringContainsInOrder(['[✗]', 'error']))),
+        ).called(1);
+
+        verify(
+          () => logger.info(any(that: contains('2 issues detected.'))),
+        ).called(1);
+      });
+
+      group('validator scope', () {
+        const appId = 'test-app-id';
+        late Validator projectScopeValidator;
+        late Validator installationScopeValidator;
+
+        Directory setUpTempDir() {
+          final tempDir = Directory.systemTemp.createTempSync();
+          File(
+            p.join(tempDir.path, 'shorebird.yaml'),
+          ).writeAsStringSync('app_id: $appId');
+          return tempDir;
+        }
+
+        setUp(() {
+          projectScopeValidator = _MockValidator();
+          installationScopeValidator = _MockValidator();
+
+          when(projectScopeValidator.validate).thenAnswer((_) async => []);
+          when(() => projectScopeValidator.scope)
+              .thenReturn(ValidatorScope.project);
+          when(() => projectScopeValidator.description)
+              .thenReturn('project-scoped validator');
+
+          when(installationScopeValidator.validate).thenAnswer((_) async => []);
+          when(() => installationScopeValidator.scope)
+              .thenReturn(ValidatorScope.installation);
+          when(() => installationScopeValidator.description)
+              .thenReturn('installation-scoped validator');
+        });
+
+        test('does not run project-scoped validators in project directory',
+            () async {
+          final validators = [
+            projectScopeValidator,
+            installationScopeValidator
+          ];
+          await runWithOverrides(
+            () => doctor.runValidators(validators),
+          );
+          verify(installationScopeValidator.validate).called(1);
+          verifyNever(projectScopeValidator.validate);
+        });
+
+        test('runs project-scoped validators in project directory', () async {
+          final tempDir = setUpTempDir();
+          final validators = [
+            projectScopeValidator,
+            installationScopeValidator
+          ];
+          await runWithOverrides(
+            () async => IOOverrides.runZoned(
+              () => doctor.runValidators(validators),
+              getCurrentDirectory: () => tempDir,
+            ),
+          );
+          verify(installationScopeValidator.validate).called(1);
+          verify(projectScopeValidator.validate).called(1);
+        });
+      });
+
+      group('fix', () {
+        var wasFixCalled = false;
+        final fixableValidationWarning = ValidationIssue(
+          severity: ValidationIssueSeverity.warning,
+          message: 'warning',
+          fix: () => wasFixCalled = true,
+        );
+        final erroringValidationWarning = ValidationIssue(
+          severity: ValidationIssueSeverity.warning,
+          message: 'warning',
+          fix: () => throw Exception('oh no!'),
+        );
+
+        late Validator fixableWarningValidator;
+
+        setUp(() {
+          wasFixCalled = false;
+          fixableWarningValidator = _MockValidator();
+
+          when(fixableWarningValidator.validate).thenAnswer(
+            (_) async => [fixableValidationWarning],
+          );
+          when(() => fixableWarningValidator.scope)
+              .thenReturn(ValidatorScope.installation);
+          when(() => fixableWarningValidator.description)
+              .thenReturn('fixable warning validator');
+        });
+
+        test(
+            '''does not tell the user we can fix issues if no fixable issues are found''',
+            () async {
+          await runWithOverrides(
+            () => doctor.runValidators([warningValidator, errorValidator]),
+          );
+
+          verifyNever(
+            () => logger.info(
+              any(
+                that: stringContainsInOrder([
+                  'can be fixed automatically with',
+                  'shorebird doctor --fix',
+                ]),
+              ),
+            ),
+          );
+        });
+
+        test('does not perform fixes if applyFixes is false', () async {
+          await runWithOverrides(
+            () => doctor.runValidators([fixableWarningValidator]),
+          );
+
+          verify(
+            () => logger.info(
+              any(
+                that: stringContainsInOrder([
+                  'can be fixed automatically with',
+                  'shorebird doctor --fix',
+                ]),
+              ),
+            ),
+          ).called(1);
+          expect(wasFixCalled, isFalse);
+        });
+
+        test('performs fixes if applyFixes is true', () async {
+          when(fixableWarningValidator.validate).thenAnswer(
+            (_) async => wasFixCalled ? [] : [fixableValidationWarning],
+          );
+          await runWithOverrides(
+            () async => doctor.runValidators(
+              [fixableWarningValidator],
+              applyFixes: true,
+            ),
+          );
+
+          verify(() => progress.update('Fixing'));
+          verify(
+            () => progress.complete(
+              'fixable warning validator ${green.wrap('(1 fix applied)')}',
+            ),
+          ).called(1);
+          expect(wasFixCalled, isTrue);
+        });
+
+        test('prints error if fixes fail to apply', () async {
+          when(fixableWarningValidator.validate).thenAnswer(
+            (_) async => [erroringValidationWarning],
+          );
+          await runWithOverrides(
+            () async => doctor.runValidators(
+              [fixableWarningValidator],
+              applyFixes: true,
+            ),
+          );
+
+          verify(
+            () => logger.err(
+              '''  An error occurred while attempting to fix warning: Exception: oh no!''',
+            ),
+          ).called(1);
+        });
+      });
+    });
+  });
+}
