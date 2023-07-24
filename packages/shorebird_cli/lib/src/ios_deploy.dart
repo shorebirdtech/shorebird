a81986ca026f024fe99ba10a3e59e341c69e106f
Felix Angelov <felix@shorebird.dev>
2023-07-24 17:18:47 -0500

    feat(shorebird_cli): add `IOSDeploy`

---
+++ packages/shorebird_cli/lib/src/ios_deploy.dart
@@ -0,0 +1,35 @@
+import 'package:path/path.dart' as p;
+import 'package:shorebird_cli/src/process.dart';
+import 'package:shorebird_cli/src/shorebird_environment.dart';
+
+/// Wrapper around the `ios-deploy` command cached by the Flutter tool.
+/// https://github.com/ios-control/ios-deploy
+class IOSDeploy {
+  /// Installs the specified app bundle.
+  ///
+  /// Uses ios-deploy and returns the exit code.
+  /// `ios-deploy --id [deviceId] --bundle [bundlePath]`
+  Future<int> installApp({
+    required String deviceId,
+    required String bundlePath,
+  }) async {
+    final iosDeployExecutable = p.join(
+      ShorebirdEnvironment.flutterDirectory.path,
+      'bin',
+      'cache',
+      'artifacts',
+      'ios-deploy',
+      'ios-deploy',
+    );
+    final result = await process.run(
+      iosDeployExecutable,
+      [
+        '--id',
+        deviceId,
+        '--bundle',
+        bundlePath,
+      ],
+    );
+    return result.exitCode;
+  }
+}
