baa375ebb9e118b36ab663edae2e3a307fdd0a19
Felix Angelov <felix@shorebird.dev>
2023-07-25 14:34:42 -0500

    refactor(shorebird_cli): adjust `IOSDeploy` to `installAndLaunchApp` (#907)

--- packages/shorebird_cli/lib/src/ios_deploy.dart
+++ packages/shorebird_cli/lib/src/ios_deploy.dart
@@ -5,13 +5,15 @@ import 'package:shorebird_cli/src/shorebird_environment.dart';
 /// Wrapper around the `ios-deploy` command cached by the Flutter tool.
 /// https://github.com/ios-control/ios-deploy
 class IOSDeploy {
-  /// Installs the .app file at [bundlePath] to the device identified by [deviceId].
+  /// Installs the .app file at [bundlePath]
+  /// to the device identified by [deviceId]
+  /// and attaches the debugger.
   ///
   /// Uses ios-deploy and returns the exit code.
-  /// `ios-deploy --id [deviceId] --bundle [bundlePath]`
-  Future<int> installApp({
-    required String deviceId,
+  /// `ios-deploy --id [deviceId] --debug --bundle [bundlePath]`
+  Future<int> installAndLaunchApp({
     required String bundlePath,
+    String? deviceId,
   }) async {
     final iosDeployExecutable = p.join(
       ShorebirdEnvironment.flutterDirectory.path,
@@ -24,8 +26,8 @@ class IOSDeploy {
     final result = await process.run(
       iosDeployExecutable,
       [
-        '--id',
-        deviceId,
+        '--debug',
+        if (deviceId != null) ...['--id', deviceId],
         '--bundle',
         bundlePath,
       ],
