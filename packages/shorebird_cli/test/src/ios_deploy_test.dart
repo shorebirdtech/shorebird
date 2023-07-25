6630a284a2aa3776c491c29fc8064ecb88a1d8c0
Felix Angelov <felix@shorebird.dev>
2023-07-25 13:20:03 -0500

    refactor(shorebird_cli): adjust `IOSDeploy` to `installAndLaunchApp`

--- packages/shorebird_cli/test/src/ios_deploy_test.dart
+++ packages/shorebird_cli/test/src/ios_deploy_test.dart
@@ -25,32 +25,60 @@ void main() {
       iosDeploy = IOSDeploy();
     });
 
-    test('executes correct command', () async {
-      const processResult = ShorebirdProcessResult(
-        exitCode: 0,
-        stdout: '',
-        stderr: '',
-      );
-      when(
-        () => process.run(any(), any()),
-      ).thenAnswer((_) async => processResult);
-      const deviceId = 'test-device-id';
-      const bundlePath = 'test-bundle-path';
-      final result = await runWithOverrides(
-        () => iosDeploy.installApp(
-          deviceId: deviceId,
-          bundlePath: bundlePath,
-        ),
-      );
-      expect(result, equals(processResult.exitCode));
-      verify(
-        () => process.run(any(that: endsWith('ios-deploy')), [
-          '--id',
-          deviceId,
-          '--bundle',
-          bundlePath,
-        ]),
-      ).called(1);
+    group('installAndLaunchApp', () {
+      test('executes correct command w/deviceId', () async {
+        const processResult = ShorebirdProcessResult(
+          exitCode: 0,
+          stdout: '',
+          stderr: '',
+        );
+        when(
+          () => process.run(any(), any()),
+        ).thenAnswer((_) async => processResult);
+        const deviceId = 'test-device-id';
+        const bundlePath = 'test-bundle-path';
+        final result = await runWithOverrides(
+          () => iosDeploy.installAndLaunchApp(
+            deviceId: deviceId,
+            bundlePath: bundlePath,
+          ),
+        );
+        expect(result, equals(processResult.exitCode));
+        verify(
+          () => process.run(any(that: endsWith('ios-deploy')), [
+            '--id',
+            deviceId,
+            '--debug',
+            '--bundle',
+            bundlePath,
+          ]),
+        ).called(1);
+      });
+
+      test('executes correct command w/out deviceId', () async {
+        const processResult = ShorebirdProcessResult(
+          exitCode: 0,
+          stdout: '',
+          stderr: '',
+        );
+        when(
+          () => process.run(any(), any()),
+        ).thenAnswer((_) async => processResult);
+        const bundlePath = 'test-bundle-path';
+        final result = await runWithOverrides(
+          () => iosDeploy.installAndLaunchApp(
+            bundlePath: bundlePath,
+          ),
+        );
+        expect(result, equals(processResult.exitCode));
+        verify(
+          () => process.run(any(that: endsWith('ios-deploy')), [
+            '--debug',
+            '--bundle',
+            bundlePath,
+          ]),
+        ).called(1);
+      });
     });
   });
 }
