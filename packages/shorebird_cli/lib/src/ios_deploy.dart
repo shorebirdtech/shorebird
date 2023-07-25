5882414bc7ed54c0662c690b2a71980ed4272018
Bryan Oltman <bryan@shorebird.dev>
2023-07-25 15:04:19 -0400

    fix(shorebird_cli): install ios-deploy for preview if not installed

--- packages/shorebird_cli/lib/src/ios_deploy.dart
+++ packages/shorebird_cli/lib/src/ios_deploy.dart
@@ -1,13 +1,29 @@
+import 'dart:io';
+
+import 'package:mason_logger/mason_logger.dart';
 import 'package:path/path.dart' as p;
+import 'package:shorebird_cli/src/logger.dart';
 import 'package:shorebird_cli/src/process.dart';
 import 'package:shorebird_cli/src/shorebird_environment.dart';
 
 /// Wrapper around the `ios-deploy` command cached by the Flutter tool.
 /// https://github.com/ios-control/ios-deploy
 class IOSDeploy {
-  /// Installs the .app file at [bundlePath]
-  /// to the device identified by [deviceId]
-  /// and attaches the debugger.
+  File get _iosDeployExecutable => File(
+        p.join(
+          ShorebirdEnvironment.flutterDirectory.path,
+          'bin',
+          'cache',
+          'artifacts',
+          'ios-deploy',
+          'ios-deploy',
+        ),
+      );
+
+  bool get _isInstalled => _iosDeployExecutable.existsSync();
+
+  /// Installs the .app file at [bundlePath] to the device identified by
+  /// [deviceId] and attaches the debugger.
   ///
   /// Uses ios-deploy and returns the exit code.
   /// `ios-deploy --id [deviceId] --debug --bundle [bundlePath]`
@@ -15,16 +31,8 @@ class IOSDeploy {
     required String bundlePath,
     String? deviceId,
   }) async {
-    final iosDeployExecutable = p.join(
-      ShorebirdEnvironment.flutterDirectory.path,
-      'bin',
-      'cache',
-      'artifacts',
-      'ios-deploy',
-      'ios-deploy',
-    );
     final result = await process.run(
-      iosDeployExecutable,
+      _iosDeployExecutable.path,
       [
         '--debug',
         if (deviceId != null) ...['--id', deviceId],
@@ -33,5 +41,22 @@ class IOSDeploy {
       ],
     );
     return result.exitCode;
+  }
+
+  /// Installs ios-deploy if it is not already installed.
+  Future<void> installIfNeeded() async {
+    if (_isInstalled) return;
+
+    final progress = logger.progress('Installing ios-deploy');
+
+    final result = await process.run('flutter', ['precache', '--ios']);
+
+    if (result.exitCode != ExitCode.success.code || !_isInstalled) {
+      const errorMessage = 'Failed to install ios-deploy.';
+      progress.fail(errorMessage);
+      throw Exception(errorMessage);
+    }
+
+    progress.complete();
   }
 }
