41b5c111c94197212b0959dda06e1ab7ab4e6ea9
Eric Seidel <eric@shorebird.dev>
2023-07-26 12:56:03 -0700

    Add comma

--- packages/shorebird_cli/lib/src/ios.dart
+++ packages/shorebird_cli/lib/src/ios.dart
@@ -2,7 +2,8 @@ import 'package:mason_logger/mason_logger.dart';
 import 'package:shorebird_cli/src/logger.dart';
 
 void showiOSStatusWarning() {
-  final url =
-      link(uri: Uri.parse('https://docs.shorebird.dev/status#ios-alpha'));
+  final url = link(
+    uri: Uri.parse('https://docs.shorebird.dev/status#ios-alpha'),
+  );
   logger.warn('iOS support is alpha. See $url for more information.');
 }
