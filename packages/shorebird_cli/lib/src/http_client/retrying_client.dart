e34a79529f183f4c7af8b990bc7ebca555fe07b5
Bryan Oltman <bryan@shorebird.dev>
2023-07-28 14:22:04 -0400

    Tests

--- packages/shorebird_cli/lib/src/http_client/retrying_client.dart
+++ packages/shorebird_cli/lib/src/http_client/retrying_client.dart
@@ -2,18 +2,26 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:http/retry.dart';
+import 'package:meta/meta.dart';
 
 /// An http client that retries requests on connection failures.
 http.Client retryingHttpClient(http.Client client) => RetryClient(
       client,
-      when: (response) => response.statusCode >= 500,
-      whenError: (e, _) {
-        return switch (e.runtimeType) {
-          HttpException => true,
-          TlsException => true,
-          SocketException => true,
-          WebSocketException => true,
-          _ => false,
-        };
-      },
+      when: isRetryableResponse,
+      whenError: isRetryableException,
     );
+
+@visibleForTesting
+bool isRetryableException(Object exception, StackTrace _) {
+  return switch (exception.runtimeType) {
+    HttpException => true,
+    TlsException => true,
+    SocketException => true,
+    WebSocketException => true,
+    _ => false,
+  };
+}
+
+@visibleForTesting
+bool isRetryableResponse(http.BaseResponse response) =>
+    response.statusCode >= 500;
