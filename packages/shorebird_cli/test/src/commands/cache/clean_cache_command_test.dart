49a740f371ae0c2d68e43c0f8b978ba8605b26f6
bryanoltman <bryan@shorebird.dev>
2023-08-01 13:36:13 -0400

    fix(shorebird_cli): add helpful error message if cache clear fails on Windows

--- packages/shorebird_cli/test/src/commands/cache/clean_cache_command_test.dart
+++ packages/shorebird_cli/test/src/commands/cache/clean_cache_command_test.dart
@@ -1,19 +1,30 @@
+import 'dart:io';
+
 import 'package:mason_logger/mason_logger.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:path/path.dart' as p;
+import 'package:platform/platform.dart';
 import 'package:scoped/scoped.dart';
 import 'package:shorebird_cli/src/cache.dart';
 import 'package:shorebird_cli/src/commands/commands.dart';
 import 'package:shorebird_cli/src/logger.dart';
+import 'package:shorebird_cli/src/platform.dart';
 import 'package:test/test.dart';
+
+class _MockCache extends Mock implements Cache {}
 
 class _MockLogger extends Mock implements Logger {}
 
-class _MockCache extends Mock implements Cache {}
+class _MockPlatform extends Mock implements Platform {}
+
+class _MockProgress extends Mock implements Progress {}
 
 void main() {
   group('cache clean', () {
     late Cache cache;
     late Logger logger;
+    late Platform platform;
+    late Progress progress;
     late CleanCacheCommand command;
 
     R runWithOverrides<R>(R Function() body) {
@@ -22,6 +33,7 @@ void main() {
         values: {
           cacheRef.overrideWith(() => cache),
           loggerRef.overrideWith(() => logger),
+          platformRef.overrideWith(() => platform),
         },
       );
     }
@@ -29,7 +41,21 @@ void main() {
     setUp(() {
       cache = _MockCache();
       logger = _MockLogger();
+      platform = _MockPlatform();
+      progress = _MockProgress();
       command = runWithOverrides(CleanCacheCommand.new);
+
+      when(() => logger.progress(any())).thenReturn(progress);
+      when(() => platform.script).thenReturn(
+        Uri.file(
+          p.join(
+            Directory.systemTemp.createTempSync().path,
+            'bin',
+            'cache',
+            'shorebird.snapshot',
+          ),
+        ),
+      );
     });
 
     test('has a description', () {
@@ -39,8 +65,47 @@ void main() {
     test('clears the cache', () async {
       final result = await runWithOverrides(command.run);
       expect(result, equals(ExitCode.success.code));
-      verify(() => logger.success('✅ Cleared Cache!')).called(1);
+      verify(() => progress.complete('Cleared Cache')).called(1);
       verify(cache.clear).called(1);
+    });
+
+    group('on failure', () {
+      group('on Windows', () {
+        setUp(() {
+          when(() => platform.isWindows).thenReturn(true);
+        });
+
+        test('tells the user how to find the issue and exits with code 70',
+            () async {
+          when(() => cache.clear()).thenThrow(
+            const FileSystemException('Failed to delete'),
+          );
+
+          final result = await runWithOverrides(command.run);
+
+          expect(result, equals(ExitCode.software.code));
+          verify(() => progress.fail(any())).called(1);
+          verify(() => logger.info(any())).called(1);
+        });
+      });
+
+      group('on a non-Windows OS', () {
+        setUp(() {
+          when(() => platform.isWindows).thenReturn(false);
+        });
+
+        test('prints error message and exits with code 70', () async {
+          when(() => cache.clear()).thenThrow(
+            const FileSystemException('Failed to delete'),
+          );
+
+          final result = await runWithOverrides(command.run);
+
+          expect(result, equals(ExitCode.software.code));
+          verify(() => progress.fail(any())).called(1);
+          verifyNever(() => logger.info(any()));
+        });
+      });
     });
   });
 }
