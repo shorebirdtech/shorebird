d84285e2f4fd6c88c539e8dc65fd842b63a1529e
Felix Angelov <felix@shorebird.dev>
2023-08-01 10:07:24 -0500

    feat(shorebird_cli): `shorebird release ios-alpha supports --flavor and --target

--- packages/shorebird_cli/test/src/commands/release/release_ios_command_test.dart
+++ packages/shorebird_cli/test/src/commands/release/release_ios_command_test.dart
@@ -510,9 +510,13 @@ error: exportArchive: No signing certificate "iOS Distribution" found
       expect(exitCode, ExitCode.success.code);
     });
 
-    test('logs error when a flavor is supplied', () async {
+    test(
+        'succeeds when release is successful '
+        'with flavors and target', () async {
       const flavor = 'development';
+      final target = p.join('lib', 'main_development.dart');
       when(() => argResults['flavor']).thenReturn(flavor);
+      when(() => argResults['target']).thenReturn(target);
       final tempDir = setUpTempDir();
       File(
         p.join(tempDir.path, 'shorebird.yaml'),
@@ -526,70 +530,29 @@ flavors:
         getCurrentDirectory: () => tempDir,
       );
 
-      expect(exitCode, ExitCode.unavailable.code);
-
+      verify(() => logger.success('\n✅ Published Release!')).called(1);
       verify(
-        () => logger.err('''
-iOS flavors are not yet supported.
-Watch the following issue for updates: https://github.com/shorebirdtech/shorebird/issues/910'''),
+        () => logger.info(
+          any(
+            that: stringContainsInOrder(
+              [
+                'Your next step is to upload the ipa to App Store Connect.',
+                'build/ios/ipa/app_bundle_name.ipa',
+              ],
+            ),
+          ),
+        ),
       ).called(1);
-      verifyNever(
+      verify(
         () => codePushClientWrapper.createIosReleaseArtifacts(
           appId: appId,
           releaseId: release.id,
           ipaPath: any(named: 'ipaPath', that: endsWith('.ipa')),
           runnerPath: any(named: 'runnerPath', that: endsWith('Runner.app')),
         ),
-      );
+      ).called(1);
+      expect(exitCode, ExitCode.success.code);
     });
-
-    test(
-      'succeeds when release is successful '
-      'with flavors and target',
-      () async {
-        const flavor = 'development';
-        final target = p.join('lib', 'main_development.dart');
-        when(() => argResults['flavor']).thenReturn(flavor);
-        when(() => argResults['target']).thenReturn(target);
-        final tempDir = setUpTempDir();
-        File(
-          p.join(tempDir.path, 'shorebird.yaml'),
-        ).writeAsStringSync('''
-app_id: productionAppId
-flavors:
-  development: $appId''');
-
-        final exitCode = await IOOverrides.runZoned(
-          () => runWithOverrides(command.run),
-          getCurrentDirectory: () => tempDir,
-        );
-
-        verify(() => logger.success('\n✅ Published Release!')).called(1);
-        verify(
-          () => logger.info(
-            any(
-              that: stringContainsInOrder(
-                [
-                  'Your next step is to upload the ipa to App Store Connect.',
-                  'build/ios/ipa/app_bundle_name.ipa',
-                ],
-              ),
-            ),
-          ),
-        ).called(1);
-        verify(
-          () => codePushClientWrapper.createIosReleaseArtifacts(
-            appId: appId,
-            releaseId: release.id,
-            ipaPath: any(named: 'ipaPath', that: endsWith('.ipa')),
-            runnerPath: any(named: 'runnerPath', that: endsWith('Runner.app')),
-          ),
-        ).called(1);
-        expect(exitCode, ExitCode.success.code);
-      },
-      // TODO(felangel): revert when we support flavors for iOS
-      skip: true,
-    );
 
     test('does not create new release if existing release is present',
         () async {
