263265519f5bcd3fccd5d7852d6331469bf8ad80
Felix Angelov <felix@shorebird.dev>
2023-08-03 16:30:31 -0500

    docs(shorebird_cli): improve `shorebird.yaml` inline doc comments

--- packages/shorebird_cli/lib/src/commands/init_command.dart
+++ packages/shorebird_cli/lib/src/commands/init_command.dart
@@ -217,13 +217,20 @@ For more information about Shorebird, visit ${link(uri: Uri.parse('https://shore
     Map<String, String>? flavors,
   }) {
     const content = '''
-# This file is used to configure the Shorebird updater used by your application.
-# Learn more at https://shorebird.dev
+# This file is used to configure the Shorebird updater used by your app.
+# Learn more at https://docs.shorebird.dev
 # This file should be checked into version control.
 
 # This is the unique identifier assigned to your app.
-# It is used by your app to request the correct patches from Shorebird servers.
+# Your app_id is not a secret and is just used to identify your app
+# when requesting patches from Shorebird's servers.
 app_id:
+
+# auto_update controls if Shorebird should automatically update in the background on launch.
+# If auto_update: false, you will need to use package:shorebird_code_push to trigger updates.
+# https://pub.dev/packages/shorebird_code_push
+# Uncomment the following line to disable automatic updates.
+# auto_update: false
 ''';
 
     final editor = YamlEditor(content)..update(['app_id'], appId);
