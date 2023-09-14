1679cb1bbdf056e1bb5ac6940bcf99d26c34379a
Felix Angelov <felix@shorebird.dev>
2023-09-14 12:54:44 -0500

    lint

--- packages/redis_client/lib/src/redis_client.dart
+++ packages/redis_client/lib/src/redis_client.dart
@@ -246,7 +246,8 @@ class RedisClient {
 }
 
 /// {@template redis_json}
-/// An object that adds support for getting and setting JSON values. Backed by the RedisJSON module.
+/// An object that adds support for getting and setting JSON values.
+/// Backed by the RedisJSON module.
 /// https://redis.io/docs/data-types/json/
 /// {@endtemplate}
 class RedisJson {
