bb5c94a725d14802bb13598638fa68448e02acec
Eric Seidel <eric@shorebird.dev>
2023-09-22 14:39:38 -0700

    chore: Move cutler to build_engine repo

--- packages/cutler/lib/logger.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mason_logger/mason_logger.dart';
-import 'package:scoped/scoped.dart';
-
-/// A reference to a [Logger] instance.
-final loggerRef = create(Logger.new);
-
-/// The [Logger] instance available in the current zone.
-Logger get logger => read(loggerRef);
