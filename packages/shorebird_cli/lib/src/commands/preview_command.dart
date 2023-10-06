6045b1e232181102c9c49ef83b10e4fd917ebe03
Bryan Oltman <bryan@shorebird.dev>
2023-10-05 21:51:54 -0400

    remove unused import

--- packages/shorebird_cli/lib/src/commands/preview_command.dart
+++ packages/shorebird_cli/lib/src/commands/preview_command.dart
@@ -12,7 +12,6 @@ import 'package:shorebird_cli/src/cache.dart';
 import 'package:shorebird_cli/src/code_push_client_wrapper.dart';
 import 'package:shorebird_cli/src/command.dart';
 import 'package:shorebird_cli/src/deployment_track.dart';
-import 'package:shorebird_cli/src/executables/devicectl/apple_device.dart';
 import 'package:shorebird_cli/src/executables/executables.dart';
 import 'package:shorebird_cli/src/http_client/http_client.dart';
 import 'package:shorebird_cli/src/logger.dart';
