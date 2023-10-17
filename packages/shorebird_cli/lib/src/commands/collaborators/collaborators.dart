19866b2c64c84cdca05af89ac652192c14d62412
Bryan Oltman <bryan@shorebird.dev>
2023-10-17 17:26:49 -0400

    chore(shorebird_cli): delete collaborators functionality

--- packages/shorebird_cli/lib/src/commands/collaborators/collaborators.dart
+++
@@ -1,4 +0,0 @@
-export 'add_collaborators_command.dart';
-export 'collaborators_command.dart';
-export 'delete_collaborators_command.dart';
-export 'list_collaborators_command.dart';
--- packages/shorebird_cli/lib/src/commands/commands.dart
+++ packages/shorebird_cli/lib/src/commands/commands.dart
@@ -1,7 +1,6 @@
 export 'apps/apps.dart';
 export 'build/build_command.dart';
 export 'cache/cache.dart';
-export 'collaborators/collaborators.dart';
 export 'doctor_command.dart';
 export 'flutter/flutter.dart';
 export 'init_command.dart';
