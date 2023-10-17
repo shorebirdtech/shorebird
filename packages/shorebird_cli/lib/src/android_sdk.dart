0a3a3be4e09c8a6354dafe22634729f63d8f34ec
Bryan Oltman <bryan@shorebird.dev>
2023-10-17 15:25:48 -0400

    refactor

--- packages/shorebird_cli/lib/src/android_sdk.dart
+++ packages/shorebird_cli/lib/src/android_sdk.dart
@@ -30,52 +30,84 @@ class AndroidSdk {
   ///   - build-tools/$version/aapt
   ///   - platform-tools/adb
   String? get path {
-    final candidatePaths = <String>[];
-    if (platform.environment.containsKey(kAndroidHome)) {
-      candidatePaths.add(platform.environment[kAndroidHome]!);
-    }
+    final candidatePathLookups = <String? Function()>[
+      () => platform.environment[kAndroidHome],
+      () => platform.environment[kAndroidSdkRoot],
+      _defaultAndroidSdkPath,
+      _androidSdkPathFromAapt,
+      _androidSdkPathFromAdb,
+    ];
 
-    if (platform.environment.containsKey(kAndroidSdkRoot)) {
-      candidatePaths.add(platform.environment[kAndroidSdkRoot]!);
-    }
-
-    final home = platform.isWindows
-        ? platform.environment['USERPROFILE']
-        : platform.environment['HOME'];
-
-    if (home != null) {
-      if (platform.isWindows) {
-        candidatePaths.add(p.join(home, 'AppData', 'Local', 'Android', 'Sdk'));
-      } else if (platform.isLinux) {
-        candidatePaths.add(p.join(home, 'Android', 'Sdk'));
-      } else if (platform.isMacOS) {
-        candidatePaths.add(p.join(home, 'Library', 'Android', 'sdk'));
+    for (final lookup in candidatePathLookups) {
+      final maybePath = lookup();
+      if (maybePath != null && _isValidSdkPath(maybePath)) {
+        return maybePath;
       }
     }
 
-    final maybeAaptPath = osInterface.which('aapt');
-    if (maybeAaptPath != null) {
-      // Resolve path to aapt if it is a symlink.
-      final resolvedAaptPath = File(maybeAaptPath).resolveSymbolicLinksSync();
-      candidatePaths.add(File(resolvedAaptPath).parent.parent.parent.path);
-    }
-
-    final maybeAdbPath = osInterface.which('adb');
-    if (maybeAdbPath != null) {
-      // Resolve path to adb if it is a symlink.
-      final resolvedAdbPath = File(maybeAdbPath).resolveSymbolicLinksSync();
-      candidatePaths.add(File(resolvedAdbPath).parent.parent.path);
-    }
-
-    return candidatePaths.firstWhereOrNull(_isValidSdkPath);
+    return null;
   }
 
+  /// For a path to be a valid Android SDK, it must exist and have either a
+  /// licenses directory or a platform-tools directory. If the SDK only has a
+  /// licenses directory, that indicates that the user has accepted the
+  /// necessary Android licenses and that additional components can be
+  /// downloaded later.
   bool _isValidSdkPath(String path) {
     final directory = Directory(path);
     final licensesDirectory = Directory(p.join(path, 'licenses'));
     final platformToolsDirectory = Directory(p.join(path, 'platform-tools'));
     return directory.existsSync() &&
         (licensesDirectory.existsSync() || platformToolsDirectory.existsSync());
+  }
+
+  /// Returns the default path to the Android SDK if a home directory is defined
+  /// and we're on a recognized platform, or null otherwise.
+  String? _defaultAndroidSdkPath() {
+    final home = platform.isWindows
+        ? platform.environment['USERPROFILE']
+        : platform.environment['HOME'];
+    if (home == null) {
+      return null;
+    }
+
+    if (platform.isWindows) {
+      return p.join(home, 'AppData', 'Local', 'Android', 'Sdk');
+    } else if (platform.isLinux) {
+      return p.join(home, 'Android', 'Sdk');
+    } else if (platform.isMacOS) {
+      return p.join(home, 'Library', 'Android', 'sdk');
+    }
+
+    return null;
+  }
+
+  /// If adb is on the user's path, return the Android SDK that contains it.
+  ///
+  /// In a valid Android SDK, adb is in the platform-tools directory.
+  String? _androidSdkPathFromAdb() {
+    final maybeAdbPath = osInterface.which('adb');
+    if (maybeAdbPath == null) {
+      return null;
+    }
+
+    // Resolve path to adb if it is a symlink.
+    final resolvedAdbPath = File(maybeAdbPath).resolveSymbolicLinksSync();
+    return File(resolvedAdbPath).parent.parent.path;
+  }
+
+  /// If aapt is on the user's path, return the Android SDK that contains it.
+  ///
+  /// In a valid Android SDK, aapt is in the build-tools/$version directory.
+  String? _androidSdkPathFromAapt() {
+    final maybeAaptPath = osInterface.which('aapt');
+    if (maybeAaptPath == null) {
+      return null;
+    }
+
+    // Resolve path to aapt if it is a symlink.
+    final resolvedAaptPath = File(maybeAaptPath).resolveSymbolicLinksSync();
+    return File(resolvedAaptPath).parent.parent.parent.path;
   }
 
   /// The path to the `adb` executable.
