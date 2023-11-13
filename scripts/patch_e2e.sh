7ad751c14346be7721f61f96cf22936429c38bcd
Felix Angelov <felix@shorebird.dev>
2023-11-13 12:19:20 -0600

    fix

--- scripts/patch_e2e.sh
+++ scripts/patch_e2e.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -ex
 
+# This script tests the patching functionality of Shorebird.
+# It creates a new empty, flutter project, initializes Shorebird,
+# creates a new release, patches the release, and then ensures
+# that the patch was applied correctly.
+#
+# Pre-requisites:
+# - Flutter must be installed.
+# - Android SDK must be installed.
+# - ADB must be installed and be part of PATH.
+# - Android emulator must be running.
+# - Shorebird must be installed.
+#
+# Usage: ./patch_e2e.sh
+
 # Create a new empty flutter project
 flutter create e2e_test --empty --platforms android
 cd e2e_test
@@ -20,7 +34,7 @@ shorebird release android --force -v
 while IFS= read -r line; do
     if [[ "$line" == *"I flutter : hello world"* ]]; then
         adb kill-server
-        echo "✅ Test passed!"
+        echo "✅ 'hello world' was printed"
         break
     fi
 done < <(shorebird preview --release-version 0.1.0+1 --app-id $APP_ID --platform android)
@@ -36,7 +50,7 @@ while IFS= read -r line; do
     if [[ "$line" == *"I flutter : hello world"* ]]; then
         sleep 5 # Wait for the patch to be installed.
         adb kill-server
-        echo "✅ Test passed!"
+        echo "✅ 'hello world' was printed"
         break
     fi
 done < <(shorebird preview --release-version 0.1.0+1 --app-id $APP_ID --platform android)
@@ -45,7 +59,7 @@ done < <(shorebird preview --release-version 0.1.0+1 --app-id $APP_ID --platform
 while IFS= read -r line; do
     if [[ "$line" == *"I flutter : hello shorebird"* ]]; then
         adb kill-server
-        echo "✅ Test passed!"
+        echo "✅ 'hello shorebird' was printed"
         break
     fi
 done < <(shorebird preview --release-version 0.1.0+1 --app-id $APP_ID --platform android)
