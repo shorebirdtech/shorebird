bf28d1edb6e804a8fdfce9823fcc2adde76e7fbd
Felix Angelov <felix@shorebird.dev>
2023-11-16 11:54:04 -0600

    feat(shorebird_cli): add `AotTools`

---
+++ packages/shorebird_cli/lib/src/executables/aot_tools.dart
@@ -0,0 +1,44 @@
+import 'package:path/path.dart' as p;
+import 'package:scoped/scoped.dart';
+import 'package:shorebird_cli/src/cache.dart';
+import 'package:shorebird_cli/src/process.dart';
+
+/// A reference to a [AotTools] instance.
+final aotToolsRef = create(AotTools.new);
+
+/// The [AotTools] instance available in the current zone.
+AotTools get aotTools => read(aotToolsRef);
+
+/// Wrapper around the shorebird `aot-tools` executable.
+class AotTools {
+  static const executableName = 'aot-tools';
+
+  Future<ShorebirdProcessResult> _exec(List<String> command) async {
+    await cache.updateAll();
+    final executable = p.join(
+      cache.getArtifactDirectory(executableName).path,
+      executableName,
+    );
+
+    return process.run(executable, command);
+  }
+
+  /// Generate a link vmcode file from two AOT snapshots.
+  Future<void> link({
+    required String base,
+    required String patch,
+    required String analyzeSnapshot,
+  }) async {
+    final result = await _exec(
+      [
+        'link',
+        '--base=$base',
+        '--patch=$patch',
+        '--analyze-snapshot=$analyzeSnapshot',
+      ],
+    );
+    if (result.exitCode != 0) {
+      throw Exception('Failed to link: ${result.stderr}');
+    }
+  }
+}
