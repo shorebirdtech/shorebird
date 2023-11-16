fa69c6e3681f073239a022b138cf814d1f33fb2c
Felix Angelov <felix@shorebird.dev>
2023-11-16 14:53:20 -0600

    feat(shorebird_cli): add `workingDirectory` to `AotTools.link`

--- packages/shorebird_cli/lib/src/executables/aot_tools.dart
+++ packages/shorebird_cli/lib/src/executables/aot_tools.dart
@@ -6,14 +6,17 @@ import 'package:shorebird_cli/src/process.dart';
 class AotTools {
   static const executableName = 'aot-tools';
 
-  Future<ShorebirdProcessResult> _exec(List<String> command) async {
+  Future<ShorebirdProcessResult> _exec(
+    List<String> command, {
+    String? workingDirectory,
+  }) async {
     await cache.updateAll();
     final executable = p.join(
       cache.getArtifactDirectory(executableName).path,
       executableName,
     );
 
-    return process.run(executable, command);
+    return process.run(executable, command, workingDirectory: workingDirectory);
   }
 
   /// Generate a link vmcode file from two AOT snapshots.
@@ -21,6 +24,7 @@ class AotTools {
     required String base,
     required String patch,
     required String analyzeSnapshot,
+    String? workingDirectory,
   }) async {
     final result = await _exec(
       [
@@ -29,7 +33,9 @@ class AotTools {
         '--patch=$patch',
         '--analyze-snapshot=$analyzeSnapshot',
       ],
+      workingDirectory: workingDirectory,
     );
+
     if (result.exitCode != 0) {
       throw Exception('Failed to link: ${result.stderr}');
     }
