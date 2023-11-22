4cb9bf1930114ffa77908ff88b94a377c0c25e52
Bryan Oltman <bryan@shorebird.dev>
2023-11-22 12:19:05 -0500

    fix(shorebird_cli): download from storage bucket to verify access to storage.googleapis.com

--- packages/shorebird_cli/test/src/validators/storage_access_validator_test.dart
+++ packages/shorebird_cli/test/src/validators/storage_access_validator_test.dart
@@ -1,9 +1,9 @@
-import 'package:mason_logger/mason_logger.dart';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
-import 'package:platform/platform.dart';
 import 'package:scoped/scoped.dart';
-import 'package:shorebird_cli/src/platform.dart';
-import 'package:shorebird_cli/src/process.dart';
+import 'package:shorebird_cli/src/http_client/http_client.dart';
 import 'package:shorebird_cli/src/validators/validators.dart';
 import 'package:test/test.dart';
 
@@ -11,31 +11,29 @@ import '../mocks.dart';
 
 void main() {
   group(StorageAccessValidator, () {
-    late Platform platform;
-    late ShorebirdProcess process;
-    late ShorebirdProcessResult pingProcessResult;
+    late http.Client httpClient;
     late StorageAccessValidator validator;
 
     R runWithOverrides<R>(R Function() body) {
       return runScoped(
         () => body(),
         values: {
-          platformRef.overrideWith(() => platform),
-          processRef.overrideWith(() => process),
+          httpClientRef.overrideWith(() => httpClient),
         },
       );
     }
 
+    setUpAll(() {
+      registerFallbackValue(Uri.parse('http://example.com'));
+    });
+
     setUp(() {
-      platform = MockPlatform();
-      process = MockShorebirdProcess();
-      pingProcessResult = MockShorebirdProcessResult();
+      httpClient = MockHttpClient();
       validator = StorageAccessValidator();
 
-      when(() => process.run('ping', any())).thenAnswer(
-        (_) async => pingProcessResult,
+      when(() => httpClient.get(any())).thenAnswer(
+        (_) async => http.Response('', HttpStatus.ok),
       );
-      when(() => platform.isWindows).thenReturn(false);
     });
 
     group('description', () {
@@ -47,8 +45,9 @@ void main() {
     group('validate', () {
       group('when storage url is accessible', () {
         setUp(() {
-          when(() => pingProcessResult.exitCode)
-              .thenReturn(ExitCode.success.code);
+          when(() => httpClient.get(any())).thenAnswer(
+            (_) async => http.Response('', HttpStatus.ok),
+          );
         });
 
         test('returns empty list of validation issues', () async {
@@ -59,8 +58,9 @@ void main() {
 
       group('when storage url is inaccessible', () {
         setUp(() {
-          when(() => pingProcessResult.exitCode)
-              .thenReturn(ExitCode.software.code);
+          when(() => httpClient.get(any())).thenAnswer(
+            (_) async => http.Response('Not Found', HttpStatus.notFound),
+          );
         });
 
         test('returns validation error', () async {
@@ -76,42 +76,6 @@ void main() {
               ],
             ),
           );
-        });
-      });
-
-      group('when run on Windows', () {
-        setUp(() {
-          when(() => pingProcessResult.exitCode)
-              .thenReturn(ExitCode.success.code);
-          when(() => platform.isWindows).thenReturn(true);
-        });
-
-        test('provides appropriate count argument', () async {
-          await runWithOverrides(validator.validate);
-          verify(
-            () => process.run(
-              'ping',
-              ['/n', '1', 'storage.googleapis.com'],
-            ),
-          ).called(1);
-        });
-      });
-
-      group('when run on non-Windows OS', () {
-        setUp(() {
-          when(() => pingProcessResult.exitCode)
-              .thenReturn(ExitCode.success.code);
-          when(() => platform.isWindows).thenReturn(false);
-        });
-
-        test('provides appropriate count argument', () async {
-          await runWithOverrides(validator.validate);
-          verify(
-            () => process.run(
-              'ping',
-              ['-c', '1', 'storage.googleapis.com'],
-            ),
-          ).called(1);
         });
       });
     });
