461bcd5455677ad7e46d536911b9c629d62830bf
Bryan Oltman <bryan@shorebird.dev>
2024-02-20 17:16:17 -0500

    merge

--- packages/jwt/lib/src/encoding.dart
+++ packages/jwt/lib/src/encoding.dart
@@ -5,19 +5,5 @@ Map<String, dynamic> decodeJwtPart(String part) {
   final normalized = base64.normalize(part);
   final base64Decoded = base64.decode(normalized);
   final utf8Decoded = utf8.decode(base64Decoded);
-  final jsonDecoded = json.decode(utf8Decoded) as Map<String, dynamic>;
-  return jsonDecoded;
-}
-
-String base64Padded(String value) {
-  final mod = value.length % 4;
-  if (mod == 0) {
-    return value;
-  } else if (mod == 3) {
-    return value.padRight(value.length + 1, '=');
-  } else if (mod == 2) {
-    return value.padRight(value.length + 2, '=');
-  } else {
-    return value; // let it fail when decoding
-  }
+  return json.decode(utf8Decoded) as Map<String, dynamic>;
 }
