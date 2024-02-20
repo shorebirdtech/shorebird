021cda2ae5f928d11a9637448feba837bb0c7961
Bryan Oltman <bryan@shorebird.dev>
2024-02-20 15:14:57 -0500

    tweak

--- packages/jwt/lib/src/encoding.dart
+++ packages/jwt/lib/src/encoding.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 /// Converts one of the three base64-encoded parts of a JWT to a JSON object.
-Map<String, dynamic> decodedJwtPart(String part) {
+Map<String, dynamic> decodeJwtPart(String part) {
   final normalized = base64.normalize(part);
   final base64Decoded = base64.decode(normalized);
   final utf8Decoded = utf8.decode(base64Decoded);
--- packages/jwt/lib/src/models/jwt.dart
+++ packages/jwt/lib/src/models/jwt.dart
@@ -23,14 +23,14 @@ class Jwt {
 
     final JwtHeader header;
     try {
-      header = JwtHeader.fromJson(decodedJwtPart(parts[0]));
+      header = JwtHeader.fromJson(decodeJwtPart(parts[0]));
     } catch (_) {
       throw const FormatException('JWT header is malformed.');
     }
 
     final JwtPayload payload;
     try {
-      payload = JwtPayload.fromJson(decodedJwtPart(parts[1]));
+      payload = JwtPayload.fromJson(decodeJwtPart(parts[1]));
     } catch (_) {
       throw const FormatException('JWT payload is malformed.');
     }
@@ -41,7 +41,7 @@ class Jwt {
       header: header,
       payload: payload,
       signature: signature,
-      claims: decodedJwtPart(parts[1]),
+      claims: decodeJwtPart(parts[1]),
     );
   }
 
