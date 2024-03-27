887ee2fcae119542bc980d66c7496d43e7044407
Bryan Oltman <bryan@shorebird.dev>
2024-03-27 11:32:14 -0400

    coverage

---
+++ packages/shorebird_code_push_protocol/test/src/models/create_patch_metadata_test.dart
@@ -0,0 +1,26 @@
+import 'package:shorebird_code_push_protocol/shorebird_code_push_protocol.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(CreatePatchMetadata, () {
+    test('can be (de)serialized', () {
+      const metadata = CreatePatchMetadata(
+        releasePlatform: ReleasePlatform.android,
+        usedIgnoreAssetChangesFlag: false,
+        hasAssetChanges: false,
+        usedIgnoreNativeChangesFlag: false,
+        hasNativeChanges: false,
+        environment: BuildEnvironmentMetadata(
+          operatingSystem: 'macos',
+          operatingSystemVersion: '1.2.3',
+          shorebirdVersion: '4.5.6',
+          xcodeVersion: '15.0',
+        ),
+      );
+      expect(
+        CreatePatchMetadata.fromJson(metadata.toJson()).toJson(),
+        equals(metadata.toJson()),
+      );
+    });
+  });
+}
