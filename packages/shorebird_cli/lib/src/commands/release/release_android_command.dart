979ede46232758d0d3f072d6ab7f0517fe234ff4
Abhijith K <abhijith.k@partner.samsung.com>
2024-04-04 18:19:29 +0530

    fix: camel case flavors can confuse path finding for android
    added kebeb case in string extenstions and used in appbuble path in android command

--- packages/shorebird_cli/lib/src/commands/release/release_android_command.dart
+++ packages/shorebird_cli/lib/src/commands/release/release_android_command.dart
@@ -9,6 +9,7 @@ import 'package:shorebird_cli/src/commands/commands.dart';
 import 'package:shorebird_cli/src/config/shorebird_yaml.dart';
 import 'package:shorebird_cli/src/doctor.dart';
 import 'package:shorebird_cli/src/extensions/arg_results.dart';
+import 'package:shorebird_cli/src/extensions/string.dart';
 import 'package:shorebird_cli/src/logger.dart';
 import 'package:shorebird_cli/src/platform.dart';
 import 'package:shorebird_cli/src/shorebird_build_mixin.dart';
@@ -195,6 +196,7 @@ Use `shorebird flutter versions list` to list available versions.
           'outputs',
           'bundle',
         );
+
         final apkDirPath = p.join(
           projectRoot.path,
           'build',
@@ -206,11 +208,11 @@ Use `shorebird flutter versions list` to list available versions.
             ? p.join(
                 bundleDirPath,
                 '${flavor}Release',
-                'app-$flavor-release.aab',
+                'app-${flavor.toKebabCase}-release.aab',
               )
             : p.join(bundleDirPath, 'release', 'app-release.aab');
         final apkPath = flavor != null
-            ? p.join(apkDirPath, flavor, 'release', 'app-$flavor-release.apk')
+            ? p.join(apkDirPath, flavor, 'release', 'app-${flavor.toKebabCase}-release.apk')
             : p.join(apkDirPath, 'release', 'app-release.apk');
 
         final String releaseVersion;
--- packages/shorebird_cli/lib/src/extensions/string.dart
+++ packages/shorebird_cli/lib/src/extensions/string.dart
@@ -1,4 +1,9 @@
 extension NullOrEmtpy on String? {
   /// Returns `true` if this string is null or empty.
   bool get isNullOrEmpty => this == null || this!.isEmpty;
+
+  String? get toKebabCase {
+    final exp = RegExp('(?<=[a-z])[A-Z]');
+    return this?.replaceAllMapped(exp, (m) => '-${m.group(0)}').toLowerCase();
+  }
 }
