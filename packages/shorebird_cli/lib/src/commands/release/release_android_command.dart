3dc991cc910277f0c2b7ac33f301534d7fa765a2
Felix Angelov <felix@shorebird.dev>
2024-04-16 14:03:04 -0500

    fix(shorebird_cli): ensure all release artifacts are built using the same flutter revision (#1911)

--- packages/shorebird_cli/lib/src/commands/release/release_android_command.dart
+++ packages/shorebird_cli/lib/src/commands/release/release_android_command.dart
@@ -245,6 +245,17 @@ Use `shorebird flutter versions list` to list available versions.
             release: existingRelease,
             platform: releasePlatform,
           );
+
+          // All artifacts associated with a given release must be built
+          // with the same Flutter revision.
+          if (existingRelease.flutterRevision != flutterRevisionForRelease) {
+            ReleaseCommand.printConflictingFlutterRevisionError(
+              existingFlutterRevision: existingRelease.flutterRevision,
+              currentFlutterRevision: flutterRevisionForRelease,
+              releaseVersion: releaseVersion,
+            );
+            return ExitCode.software.code;
+          }
         }
 
         final archNames = architectures.map((a) => a.name);
