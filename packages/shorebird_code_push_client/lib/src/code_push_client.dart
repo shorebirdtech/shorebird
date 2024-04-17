74fab18c3dbce9739d8b0fb7ae568bc998546f55
Bryan Oltman <bryan@shorebird.dev>
2024-04-17 12:31:08 -0400

    fix(shorebird_cli): don't use localhost for code push endpoint

--- packages/shorebird_code_push_client/lib/src/code_push_client.dart
+++ packages/shorebird_code_push_client/lib/src/code_push_client.dart
@@ -95,8 +95,7 @@ class CodePushClient {
           httpClient ?? http.Client(),
           {...standardHeaders, ...?customHeaders},
         ),
-        // hostedUri = hostedUri ?? Uri.https('api.shorebird.dev');
-        hostedUri = hostedUri ?? Uri.http('localhost:8080');
+        hostedUri = hostedUri ?? Uri.https('api.shorebird.dev');
 
   /// The standard headers applied to all requests.
   static const standardHeaders = <String, String>{'x-version': packageVersion};
