25b1fb212a995f2daf786483a9a7be46bf8dbc54
Felix Angelov <felix@shorebird.dev>
2024-04-23 15:25:23 -0500

    chore: update bin to include `pubspecEditorRef`

--- packages/shorebird_cli/bin/shorebird.dart
+++ packages/shorebird_cli/bin/shorebird.dart
@@ -17,6 +17,7 @@ import 'package:shorebird_cli/src/os/os.dart';
 import 'package:shorebird_cli/src/patch_diff_checker.dart';
 import 'package:shorebird_cli/src/platform.dart';
 import 'package:shorebird_cli/src/platform/platform.dart';
+import 'package:shorebird_cli/src/pubspec_editor.dart';
 import 'package:shorebird_cli/src/shorebird_artifacts.dart';
 import 'package:shorebird_cli/src/shorebird_env.dart';
 import 'package:shorebird_cli/src/shorebird_flutter.dart';
@@ -53,6 +54,7 @@ Future<void> main(List<String> args) async {
         patchDiffCheckerRef,
         platformRef,
         processRef,
+        pubspecEditorRef,
         shorebirdArtifactsRef,
         shorebirdEnvRef,
         shorebirdFlutterRef,
