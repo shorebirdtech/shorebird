30c3a44419dcd0b70025be09229812b087ffaa25
Erick Zanardo <erickzanardoo@gmail.com>
2024-04-23 17:08:48 -0300

    suggestions

--- packages/shorebird_cli/test/src/shorebird_android_artifacts_test.dart
+++ packages/shorebird_cli/test/src/shorebird_android_artifacts_test.dart
@@ -13,287 +13,294 @@ void main() {
       shorebirdAndroidArtifacts = ShorebirdAndroidArtifacts();
     });
 
-    test('throws ArtifactNotFoundException when cannot find the aab', () {
-      expect(
-        () => shorebirdAndroidArtifacts.findAppBundle(
-          project: project,
-          flavor: null,
-        ),
-        throwsA(isA<ArtifactNotFoundException>()),
-      );
+    group('when cannot find artifacts', () {
+      test('throws ArtifactNotFoundException for aabs', () {
+        expect(
+          () => shorebirdAndroidArtifacts.findAppBundle(
+            project: project,
+            flavor: null,
+          ),
+          throwsA(isA<ArtifactNotFoundException>()),
+        );
+      });
+      test('throws ArtifactNotFoundException for apks', () {
+        expect(
+          () => shorebirdAndroidArtifacts.findApk(
+            project: project,
+            flavor: null,
+          ),
+          throwsA(isA<ArtifactNotFoundException>()),
+        );
+      });
     });
 
-    test('throws ArtifactNotFoundException when cannot find the apk', () {
-      expect(
-        () => shorebirdAndroidArtifacts.findApk(
-          project: project,
-          flavor: null,
-        ),
-        throwsA(isA<ArtifactNotFoundException>()),
-      );
+    group('with no flavors', () {
+      test('finds the app bundle flavors', () {
+        final artifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'bundle',
+          'release',
+          'app-release.aab',
+        );
+        final artifact = File(
+          path.join(project.path, artifactPath),
+        )..createSync(recursive: true);
+
+        expect(
+          shorebirdAndroidArtifacts
+              .findAppBundle(
+                project: project,
+                flavor: null,
+              )
+              .path,
+          equals(artifact.path),
+        );
+      });
+      test('finds the apk', () {
+        final artifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'flutter-apk',
+          'app-release.apk',
+        );
+        final artifact = File(
+          path.join(project.path, artifactPath),
+        )..createSync(recursive: true);
+
+        expect(
+          shorebirdAndroidArtifacts
+              .findApk(
+                project: project,
+                flavor: null,
+              )
+              .path,
+          equals(artifact.path),
+        );
+      });
     });
 
-    test('finds the app bundle without any flavors', () {
-      final artifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'bundle',
-        'release',
-        'app-release.aab',
-      );
-      final artifact = File(
-        path.join(project.path, artifactPath),
-      )..createSync(recursive: true);
+    group('with one dimensional flavor', () {
+      test('finds the app bundle', () {
+        final artifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'bundle',
+          'internalRelease',
+          'app-internal-release.aab',
+        );
+        final artifact = File(
+          path.join(project.path, artifactPath),
+        )..createSync(recursive: true);
 
-      expect(
-        shorebirdAndroidArtifacts
-            .findAppBundle(
-              project: project,
-              flavor: null,
-            )
-            .path,
-        equals(artifact.path),
-      );
-    });
-    test('find the apk bundle without any flavors', () {
-      final artifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'flutter-apk',
-        'app-release.apk',
-      );
-      final artifact = File(
-        path.join(project.path, artifactPath),
-      )..createSync(recursive: true);
+        const flavor = 'internal';
 
-      expect(
-        shorebirdAndroidArtifacts
-            .findApk(
-              project: project,
-              flavor: null,
-            )
-            .path,
-        equals(artifact.path),
-      );
-    });
+        expect(
+          shorebirdAndroidArtifacts
+              .findAppBundle(
+                project: project,
+                flavor: flavor,
+              )
+              .path,
+          equals(artifact.path),
+        );
+      });
+      test('finds the apk', () {
+        final artifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'flutter-apk',
+          'app-internal-release.apk',
+        );
+        final artifact = File(
+          path.join(project.path, artifactPath),
+        )..createSync(recursive: true);
 
-    test('find the app bundle with an one dimensional flavor', () {
-      final artifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'bundle',
-        'internalRelease',
-        'app-internal-release.aab',
-      );
-      final artifact = File(
-        path.join(project.path, artifactPath),
-      )..createSync(recursive: true);
+        const flavor = 'internal';
 
-      const flavor = 'internal';
-
-      expect(
-        shorebirdAndroidArtifacts
-            .findAppBundle(
-              project: project,
-              flavor: flavor,
-            )
-            .path,
-        equals(artifact.path),
-      );
-    });
-    test('find the apk with an one dimensional flavor', () {
-      final artifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'flutter-apk',
-        'app-internal-release.apk',
-      );
-      final artifact = File(
-        path.join(project.path, artifactPath),
-      )..createSync(recursive: true);
-
-      const flavor = 'internal';
-
-      expect(
-        shorebirdAndroidArtifacts
-            .findApk(
-              project: project,
-              flavor: flavor,
-            )
-            .path,
-        equals(artifact.path),
-      );
+        expect(
+          shorebirdAndroidArtifacts
+              .findApk(
+                project: project,
+                flavor: flavor,
+              )
+              .path,
+          equals(artifact.path),
+        );
+      });
     });
 
-    test('find the app bundle with multi dimensional flavors', () {
-      final artifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'bundle',
-        'stableGlobalRelease',
-        'app-stable-global-release.aab',
-      );
-      final artifact = File(
-        path.join(project.path, artifactPath),
-      )..createSync(recursive: true);
+    group('when with multi dimensional flavors', () {
+      test('finds the app bundle with multi dimensional flavors', () {
+        final artifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'bundle',
+          'stableGlobalRelease',
+          'app-stable-global-release.aab',
+        );
+        final artifact = File(
+          path.join(project.path, artifactPath),
+        )..createSync(recursive: true);
 
-      const flavor = 'stableGlobal';
+        const flavor = 'stableGlobal';
 
-      expect(
-        shorebirdAndroidArtifacts
-            .findAppBundle(
-              project: project,
-              flavor: flavor,
-            )
-            .path,
-        equals(artifact.path),
-      );
-    });
-    test('find the apk with multi dimensional flavors', () {
-      final artifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'flutter-apk',
-        'app-stableglobal-release.apk',
-      );
-      final artifact = File(
-        path.join(project.path, artifactPath),
-      )..createSync(recursive: true);
+        expect(
+          shorebirdAndroidArtifacts
+              .findAppBundle(
+                project: project,
+                flavor: flavor,
+              )
+              .path,
+          equals(artifact.path),
+        );
+      });
+      test('finds the apk with multi dimensional flavors', () {
+        final artifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'flutter-apk',
+          'app-stableglobal-release.apk',
+        );
+        final artifact = File(
+          path.join(project.path, artifactPath),
+        )..createSync(recursive: true);
 
-      const flavor = 'stableGlobal';
+        const flavor = 'stableGlobal';
 
-      expect(
-        shorebirdAndroidArtifacts
-            .findApk(
-              project: project,
-              flavor: flavor,
-            )
-            .path,
-        equals(artifact.path),
-      );
-    });
-
-    test(
-        'find the app bundle with multi dimensional flavors and multi '
-        'word flavor name', () {
-      final artifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'bundle',
-        'stablePlayStoreRelease',
-        'app-stable-playStore-release.aab',
-      );
-      final artifact = File(
-        path.join(project.path, artifactPath),
-      )..createSync(recursive: true);
-
-      const flavor = 'stablePlayStore';
-
-      expect(
-        shorebirdAndroidArtifacts
-            .findAppBundle(
-              project: project,
-              flavor: flavor,
-            )
-            .path,
-        equals(artifact.path),
-      );
-    });
-    test(
-        'find the apk with multi dimensional flavors and multi '
-        'word flavor name', () {
-      final artifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'flutter-apk',
-        'app-stableplaystore-release.apk',
-      );
-      final artifact = File(
-        path.join(project.path, artifactPath),
-      )..createSync(recursive: true);
-
-      const flavor = 'stablePlayStore';
-
-      expect(
-        shorebirdAndroidArtifacts
-            .findApk(
-              project: project,
-              flavor: flavor,
-            )
-            .path,
-        equals(artifact.path),
-      );
+        expect(
+          shorebirdAndroidArtifacts
+              .findApk(
+                project: project,
+                flavor: flavor,
+              )
+              .path,
+          equals(artifact.path),
+        );
+      });
     });
 
-    test('throws when finding multiple aab files', () {
-      final duplicatedArtifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'bundle',
-        'stablePlayStoreRelease',
-        'app---stable-playStore-release.aab',
-      );
-      File(
-        path.join(project.path, duplicatedArtifactPath),
-      ).createSync(recursive: true);
+    group('when with multi dimensional flavors and mult word flavor name', () {
+      test('finds the app bundle', () {
+        final artifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'bundle',
+          'stablePlayStoreRelease',
+          'app-stable-playStore-release.aab',
+        );
+        final artifact = File(
+          path.join(project.path, artifactPath),
+        )..createSync(recursive: true);
 
-      const artifactPath =
-          'build/app/outputs/bundle/stablePlayStoreRelease/app-stable-playStore-release.aab';
-      File(
-        path.join(project.path, artifactPath),
-      ).createSync(recursive: true);
+        const flavor = 'stablePlayStore';
 
-      const flavor = 'stablePlayStore';
+        expect(
+          shorebirdAndroidArtifacts
+              .findAppBundle(
+                project: project,
+                flavor: flavor,
+              )
+              .path,
+          equals(artifact.path),
+        );
+      });
+      test('finds the apk', () {
+        final artifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'flutter-apk',
+          'app-stableplaystore-release.apk',
+        );
+        final artifact = File(
+          path.join(project.path, artifactPath),
+        )..createSync(recursive: true);
 
-      expect(
-        () => shorebirdAndroidArtifacts.findAppBundle(
-          project: project,
-          flavor: flavor,
-        ),
-        throwsA(isA<MultipleArtifactsFoundException>()),
-      );
+        const flavor = 'stablePlayStore';
+
+        expect(
+          shorebirdAndroidArtifacts
+              .findApk(
+                project: project,
+                flavor: flavor,
+              )
+              .path,
+          equals(artifact.path),
+        );
+      });
     });
-    test('throws when finding multiple apk files', () {
-      final duplicatedArtifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'flutter-apk',
-        'app----stableplaystore-release.apk',
-      );
-      File(
-        path.join(project.path, duplicatedArtifactPath),
-      ).createSync(recursive: true);
 
-      final artifactPath = path.join(
-        'build',
-        'app',
-        'outputs',
-        'flutter-apk',
-        'app-stableplaystore-release.apk',
-      );
-      File(
-        path.join(project.path, artifactPath),
-      ).createSync(recursive: true);
+    group('when finding multiple files', () {
+      test('throws when finding multiple aab files', () {
+        final duplicatedArtifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'bundle',
+          'stablePlayStoreRelease',
+          'app---stable-playStore-release.aab',
+        );
+        File(
+          path.join(project.path, duplicatedArtifactPath),
+        ).createSync(recursive: true);
 
-      const flavor = 'stablePlayStore';
+        const artifactPath =
+            'build/app/outputs/bundle/stablePlayStoreRelease/app-stable-playStore-release.aab';
+        File(
+          path.join(project.path, artifactPath),
+        ).createSync(recursive: true);
 
-      expect(
-        () => shorebirdAndroidArtifacts.findApk(
-          project: project,
-          flavor: flavor,
-        ),
-        throwsA(isA<MultipleArtifactsFoundException>()),
-      );
+        const flavor = 'stablePlayStore';
+
+        expect(
+          () => shorebirdAndroidArtifacts.findAppBundle(
+            project: project,
+            flavor: flavor,
+          ),
+          throwsA(isA<MultipleArtifactsFoundException>()),
+        );
+      });
+      test('throws when finding multiple apk files', () {
+        final duplicatedArtifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'flutter-apk',
+          'app----stableplaystore-release.apk',
+        );
+        File(
+          path.join(project.path, duplicatedArtifactPath),
+        ).createSync(recursive: true);
+
+        final artifactPath = path.join(
+          'build',
+          'app',
+          'outputs',
+          'flutter-apk',
+          'app-stableplaystore-release.apk',
+        );
+        File(
+          path.join(project.path, artifactPath),
+        ).createSync(recursive: true);
+
+        const flavor = 'stablePlayStore';
+
+        expect(
+          () => shorebirdAndroidArtifacts.findApk(
+            project: project,
+            flavor: flavor,
+          ),
+          throwsA(isA<MultipleArtifactsFoundException>()),
+        );
+      });
     });
   });
 }
