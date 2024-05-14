c4759d5dd5e086898a2463a450d1cc4e43a611f6
Bryan Oltman <bryan@shorebird.dev>
2024-05-13 22:07:28 -0400

    fix(shorebird_cli): only pass dump-debug-info to aot_tools if supported (#2090)

--- packages/shorebird_cli/lib/src/commands/patch/ios_patcher.dart
+++ packages/shorebird_cli/lib/src/commands/patch/ios_patcher.dart
@@ -287,7 +287,6 @@ class IosPatcher extends Patcher {
     required File releaseArtifact,
   }) async {
     final patch = File(_aotOutputPath);
-    final dumpDebugInfo = await aotTools.isLinkDebugInfoSupported();
 
     if (!patch.existsSync()) {
       logger.err('Unable to find patch AOT file at ${patch.path}');
@@ -312,7 +311,10 @@ class IosPatcher extends Patcher {
     final linkProgress = logger.progress('Linking AOT files');
     double? linkPercentage;
     try {
-      final dumpDebugInfoDir = Directory.systemTemp.createTempSync();
+      final dumpDebugInfoDir = await aotTools.isLinkDebugInfoSupported()
+          ? Directory.systemTemp.createTempSync()
+          : null;
+
       linkPercentage = await aotTools.link(
         base: releaseArtifact.path,
         patch: patch.path,
@@ -321,10 +323,10 @@ class IosPatcher extends Patcher {
         outputPath: _vmcodeOutputPath,
         workingDirectory: buildDirectory.path,
         kernel: artifactManager.newestAppDill().path,
-        dumpDebugInfoPath: dumpDebugInfoDir.path,
+        dumpDebugInfoPath: dumpDebugInfoDir?.path,
       );
 
-      if (dumpDebugInfo) {
+      if (dumpDebugInfoDir != null) {
         final debugInfoZip = await dumpDebugInfoDir.zipToTempFile();
         debugInfoZip.copySync(p.join('build', debugInfoFile.path));
       }
