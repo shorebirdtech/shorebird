71be38e6d1cec512ac1113aa036c2ce02595a9f8
Bryan Oltman <bryan@shorebird.dev>
2024-05-31 12:04:00 -0400

    feat(shorebird_cli): write all stdout and stderr to log file (#2170)

--- packages/shorebird_cli/lib/src/logger.dart
+++ packages/shorebird_cli/lib/src/logger.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:mason_logger/mason_logger.dart';
 import 'package:path/path.dart' as p;
 import 'package:scoped_deps/scoped_deps.dart';
+import 'package:shorebird_cli/src/extensions/string.dart';
 import 'package:shorebird_cli/src/shorebird_env.dart';
 
 /// A reference to a [Logger] instance.
@@ -11,93 +13,169 @@ final loggerRef = create(ShorebirdLogger.new);
 /// The [Logger] instance available in the current zone.
 ShorebirdLogger get logger => read(loggerRef);
 
-class ShorebirdLogger extends Logger {
-  ShorebirdLogger({super.level});
+const _logFileName = 'shorebird.log';
 
-  static const _logFileName = 'shorebird.log';
+/// Where logs are written for the current Shorebird CLI run. A new file will
+/// be created for every run of the Shorebird CLI, and will have the name
+/// `timestamp_shorebird.log`.
+final File currentRunLogFile = (() {
+  // TODO(bryanoltman): use package:clock to test that we use the correct timestamp
+  final timestamp = DateTime.now().millisecondsSinceEpoch;
+  final file = File(
+    p.join(
+      shorebirdEnv.logsDirectory.path,
+      '${timestamp}_$_logFileName',
+    ),
+  );
 
-  late final File logFile = (() {
-    final timestamp = DateTime.now().millisecondsSinceEpoch;
-    final file = File(
-      p.join(
-        shorebirdEnv.logsDirectory.path,
-        '${timestamp}_$_logFileName',
-      ),
-    );
+  if (!file.existsSync()) {
+    file.createSync(recursive: true);
+  }
 
-    if (!file.existsSync()) {
-      file.createSync(recursive: true);
-    }
+  return file;
+})();
 
-    return file;
-  })();
+/// Writes the given [message] to the [logFile] on its own line, prefixed with
+/// the current timestamp.
+void _writeToLogFile(Object? message, {required File logFile}) {
+  if (message == null) {
+    return;
+  }
 
-  void _logToFile(String level, String message, {required LogStyle style}) {
-    final styledMessage = style(message);
+  final timestampString = DateTime.now().toIso8601String();
+  final messageString = message.toString().removeAnsiEscapes();
+  logFile.writeAsStringSync(
+    '$timestampString $messageString\n',
+    mode: FileMode.append,
+  );
+}
 
-    logFile.writeAsStringSync(
-      '${DateTime.now().toIso8601String()} [$level] $styledMessage\n',
-      mode: FileMode.append,
-    );
+/// {@template logging_stdout}
+/// A [Stdout] implementation that logs output to a file after forwarding
+/// methods to a wrapped [Stdout] instance ([baseStdOut]). This is intended to
+/// be used as an IOOverrides.stdout implementation to log all stdout output
+/// to a file.
+///
+/// Example:
+///  ```dart
+///   final loggingStdout = LoggingStdout(baseStdOut: stdout, logFile: logFile);
+///   IOOverrides.runZoned(
+///    () => myCode(),
+///    stdout: loggingStdout,
+///   );
+/// ```
+/// {@endtemplate}
+class LoggingStdout implements Stdout {
+  /// {@macro logging_stdout}
+  LoggingStdout({required this.baseStdOut, required this.logFile});
+
+  /// The underlying [Stdout] instance that method calls are forwarded to.
+  final Stdout baseStdOut;
+
+  /// The file to which all stdout output is appended. Must exist.
+  final File logFile;
+
+  @override
+  Encoding get encoding => baseStdOut.encoding;
+
+  @override
+  set encoding(Encoding value) => baseStdOut.encoding = value;
+
+  @override
+  String get lineTerminator => baseStdOut.lineTerminator;
+
+  @override
+  set lineTerminator(String value) => baseStdOut.lineTerminator = value;
+
+  @override
+  Future<void> get done => baseStdOut.done;
+
+  @override
+  bool get hasTerminal => baseStdOut.hasTerminal;
+
+  @override
+  IOSink get nonBlocking => baseStdOut.nonBlocking;
+
+  @override
+  bool get supportsAnsiEscapes => baseStdOut.supportsAnsiEscapes;
+
+  @override
+  int get terminalColumns => baseStdOut.terminalColumns;
+
+  @override
+  int get terminalLines => baseStdOut.terminalLines;
+
+  @override
+  void add(List<int> data) {
+    baseStdOut.add(data);
+    _writeLog(String.fromCharCodes(data));
   }
 
   @override
-  void info(String? message, {LogStyle? style}) {
-    super.info(message, style: style);
-    _logToFile(
-      'INFO',
-      message ?? '',
-      style: style ?? theme.info,
-    );
+  void addError(Object error, [StackTrace? stackTrace]) {
+    baseStdOut.addError(error, stackTrace);
+
+    if (stackTrace == null) {
+      _writeLog(error);
+    } else {
+      _writeLog('$error\n$stackTrace');
+    }
   }
+
+  @override
+  Future<void> addStream(Stream<List<int>> stream) =>
+      baseStdOut.addStream(stream);
+
+  @override
+  Future<void> close() => baseStdOut.close();
+
+  @override
+  Future<void> flush() => baseStdOut.flush();
+
+  @override
+  void write(Object? object) {
+    baseStdOut.write(object);
+    _writeLog(object);
+  }
+
+  @override
+  void writeAll(Iterable<dynamic> objects, [String sep = '']) {
+    baseStdOut.writeAll(objects, sep);
+    _writeLog(objects);
+  }
+
+  @override
+  void writeCharCode(int charCode) {
+    baseStdOut.writeCharCode(charCode);
+    _writeLog(String.fromCharCode(charCode));
+  }
+
+  @override
+  void writeln([Object? object = '']) {
+    baseStdOut.writeln(object);
+    _writeLog(object);
+  }
+
+  void _writeLog(Object? object) {
+    _writeToLogFile(object, logFile: logFile);
+  }
+}
+
+/// {@template shorebird_logger}
+/// A [Logger] that
+/// {@endtemplate}
+class ShorebirdLogger extends Logger {
+  /// {@macro shorebird_logger}
+  ShorebirdLogger({super.level});
 
   @override
   void detail(String? message, {LogStyle? style}) {
     super.detail(message, style: style);
-    _logToFile(
-      'DETAIL',
-      message ?? '',
-      style: style ?? theme.detail,
-    );
-  }
-
-  @override
-  void warn(String? message, {String tag = 'WARN', LogStyle? style}) {
-    super.warn(message, tag: tag, style: style);
-    _logToFile(
-      'WARN',
-      message ?? '',
-      style: style ?? theme.warn,
-    );
-  }
-
-  @override
-  void success(String? message, {LogStyle? style}) {
-    super.success(message, style: style);
-    _logToFile(
-      'SUCCESS',
-      message ?? '',
-      style: style ?? theme.success,
-    );
-  }
-
-  @override
-  void alert(String? message, {LogStyle? style}) {
-    super.alert(message, style: style);
-    _logToFile(
-      'ALERT',
-      message ?? '',
-      style: style ?? theme.alert,
-    );
-  }
-
-  @override
-  void err(String? message, {LogStyle? style}) {
-    super.err(message, style: style);
-    _logToFile(
-      'ERROR',
-      message ?? '',
-      style: style ?? theme.err,
-    );
+    if (level.index > Level.debug.index) {
+      // We only need to write the message to the log file if this will not
+      // be written to stdout. If it is written to stdout, [LoggingStdout] will
+      // written to the log file.
+      _writeToLogFile(message ?? '', logFile: currentRunLogFile);
+    }
   }
 }
--- packages/shorebird_cli/test/src/logger_test.dart
+++ packages/shorebird_cli/test/src/logger_test.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:mason_logger/mason_logger.dart';
@@ -8,207 +9,235 @@ import 'package:shorebird_cli/src/logger.dart';
 import 'package:shorebird_cli/src/shorebird_env.dart';
 import 'package:test/test.dart';
 
+import 'fakes.dart';
 import 'mocks.dart';
 
 void main() {
-  group('ShorebirdLogger', () {
-    late Directory logsDirectory;
+  group('currentRunLogFile', () {
     late ShorebirdEnv shorebirdEnv;
-    late ShorebirdLogger shorebirdLogger;
+    late Directory logsDirectory;
 
-    setUp(() {
-      logsDirectory = Directory.systemTemp.createTempSync(
-        'shorebird_logs',
+    R runWithOverrides<R>(R Function() body) {
+      return runScoped(
+        body,
+        values: {
+          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
+        },
       );
-
-      shorebirdEnv = MockShorebirdEnv();
-      when(() => shorebirdEnv.logsDirectory).thenReturn(logsDirectory);
-
-      // Setting to quiet so we don't spam the stdout/stderr while testing
-      shorebirdLogger = ShorebirdLogger(level: Level.quiet);
-    });
-
-    String readLogFile() {
-      final logFile = Directory(p.join(logsDirectory.path)).listSync().first;
-      return File(logFile.path).readAsStringSync();
     }
 
-    test('can be instantiated', () {
-      expect(ShorebirdLogger.new, returnsNormally);
+    setUp(() {
+      logsDirectory = Directory.systemTemp.createTempSync('shorebird_logs');
+      shorebirdEnv = MockShorebirdEnv();
+      when(() => shorebirdEnv.logsDirectory).thenReturn(logsDirectory);
     });
 
-    test('info', () {
-      runScoped(
-        () {
-          shorebirdLogger.info('message');
-          expect(readLogFile(), contains('[INFO]'));
-          expect(readLogFile(), contains('message'));
-        },
+    test('creates a log file in the logs directory', () {
+      final file = runWithOverrides(() => currentRunLogFile);
+      expect(file.existsSync(), isTrue);
+      expect(file.path, startsWith(logsDirectory.path));
+    });
+  });
+
+  group(LoggingStdout, () {
+    final utf8Encoding = Encoding.getByName('utf-8')!;
+    late File logFile;
+    late LoggingStdout loggingStdout;
+    late Stdout baseStdout;
+
+    setUpAll(() {
+      registerFallbackValue(const Stream<List<int>>.empty());
+    });
+
+    setUp(() {
+      final tempDir = Directory.systemTemp.createTempSync('shorebird_logs');
+      logFile = File(p.join(tempDir.path, 'shorebird.log'));
+      baseStdout = MockStdout();
+
+      when(() => baseStdout.addStream(any())).thenAnswer((_) async {});
+      when(() => baseStdout.close()).thenAnswer((_) async {});
+      when(() => baseStdout.done).thenAnswer((_) async {});
+      when(() => baseStdout.encoding).thenReturn(utf8Encoding);
+      when(() => baseStdout.flush()).thenAnswer((_) async {});
+      when(() => baseStdout.hasTerminal).thenReturn(true);
+      when(() => baseStdout.lineTerminator).thenReturn('\n');
+      when(() => baseStdout.nonBlocking).thenReturn(FakeIOSink());
+      when(() => baseStdout.supportsAnsiEscapes).thenReturn(false);
+      when(() => baseStdout.terminalColumns).thenReturn(80);
+      when(() => baseStdout.terminalLines).thenReturn(40);
+
+      loggingStdout = LoggingStdout(baseStdOut: baseStdout, logFile: logFile);
+    });
+
+    test('encoding forwards to baseStdOut', () {
+      expect(loggingStdout.encoding, equals(utf8Encoding));
+      verify(() => baseStdout.encoding).called(1);
+    });
+
+    test('set encoding forwards to baseStdout', () {
+      final asciiEncoding = Encoding.getByName('ascii')!;
+      loggingStdout.encoding = asciiEncoding;
+      verify(() => baseStdout.encoding = asciiEncoding).called(1);
+    });
+
+    test('addStream forwards to baseStdout', () async {
+      final stream = Stream.fromIterable(['message'.codeUnits]);
+      await loggingStdout.addStream(stream);
+      verify(() => baseStdout.addStream(stream)).called(1);
+    });
+
+    test('close forwards to baseStdout', () async {
+      await loggingStdout.close();
+      verify(() => baseStdout.close()).called(1);
+    });
+
+    test('done forwards to baseStdout', () async {
+      await loggingStdout.done;
+      verify(() => baseStdout.done).called(1);
+    });
+
+    test('flush forwards to baseStdout', () async {
+      await loggingStdout.flush();
+      verify(() => baseStdout.flush()).called(1);
+    });
+
+    test('hasTerminal forwards to baseStdout', () {
+      expect(loggingStdout.hasTerminal, isTrue);
+      verify(() => baseStdout.hasTerminal).called(1);
+    });
+
+    test('lineTerminator forwards to baseStdout', () {
+      expect(loggingStdout.lineTerminator, equals('\n'));
+      verify(() => baseStdout.lineTerminator).called(1);
+    });
+
+    test('set lineTerminator forwards to baseStdout', () {
+      loggingStdout.lineTerminator = '\r\n';
+      verify(() => baseStdout.lineTerminator = '\r\n').called(1);
+    });
+
+    test('nonBlocking forwards to baseStdout', () {
+      expect(loggingStdout.nonBlocking, isA<FakeIOSink>());
+      verify(() => baseStdout.nonBlocking).called(1);
+    });
+
+    test('supportsAnsiEscapes forwards to baseStdout', () {
+      expect(loggingStdout.supportsAnsiEscapes, isFalse);
+      verify(() => baseStdout.supportsAnsiEscapes).called(1);
+    });
+
+    test('terminalColumns forwards to baseStdout', () {
+      expect(loggingStdout.terminalColumns, equals(80));
+      verify(() => baseStdout.terminalColumns).called(1);
+    });
+
+    test('terminalLines forwards to baseStdout', () {
+      expect(loggingStdout.terminalLines, equals(40));
+      verify(() => baseStdout.terminalLines).called(1);
+    });
+
+    test('add forwards to baseStdout, logs to file', () {
+      loggingStdout.add('message'.codeUnits);
+      verify(() => baseStdout.add('message'.codeUnits)).called(1);
+      expect(logFile.readAsStringSync(), contains('message'));
+    });
+
+    test('addError forwards to baseStdout, logs to file', () {
+      loggingStdout.addError('error');
+      verify(() => baseStdout.addError('error')).called(1);
+      expect(logFile.readAsStringSync(), contains('error'));
+    });
+
+    test('addError with stack trace forwards to baseStdout, logs to file', () {
+      loggingStdout.addError('error', StackTrace.current);
+      verify(() => baseStdout.addError('error', any())).called(1);
+      expect(logFile.readAsStringSync(), contains('error'));
+      expect(
+        logFile.readAsStringSync(),
+        contains('#0      main.<anonymous closure>.<anonymous closure>'),
+      );
+    });
+
+    test('write forwards to baseStdout, logs to file', () {
+      loggingStdout.write('message');
+      verify(() => baseStdout.write('message')).called(1);
+      expect(logFile.readAsStringSync(), contains('message'));
+    });
+
+    test('writeln forwards to baseStdout, logs to file', () {
+      loggingStdout.writeln('message');
+      verify(() => baseStdout.writeln('message')).called(1);
+      expect(logFile.readAsStringSync(), contains('message'));
+    });
+
+    test('writeAll forwards to baseStdout, logs to file', () {
+      loggingStdout.writeAll(['message']);
+      verify(() => baseStdout.writeAll(['message'])).called(1);
+      expect(logFile.readAsStringSync(), contains('message'));
+    });
+
+    test('writeCharCode forwards to baseStdout, logs as string to file', () {
+      loggingStdout.writeCharCode(0);
+      verify(() => baseStdout.writeCharCode(0)).called(1);
+      expect(logFile.readAsStringSync(), contains('\x00'));
+    });
+  });
+
+  group(ShorebirdLogger, () {
+    late ShorebirdEnv shorebirdEnv;
+    late ShorebirdLogger logger;
+
+    R runWithOverrides<R>(R Function() body) {
+      return runScoped(
+        body,
         values: {
           shorebirdEnvRef.overrideWith(() => shorebirdEnv),
         },
       );
+    }
+
+    setUp(() {
+      shorebirdEnv = MockShorebirdEnv();
+      when(() => shorebirdEnv.logsDirectory).thenReturn(
+        Directory.systemTemp.createTempSync(),
+      );
+      logger = ShorebirdLogger();
     });
 
-    test('info with style', () {
-      runScoped(
-        () {
-          shorebirdLogger.info(
-            'message',
-            style: (message) => message?.toUpperCase(),
+    group('detail', () {
+      group('when log level is debug or higher', () {
+        setUp(() {
+          logger.level = Level.debug;
+        });
+
+        test('does not write message to log file', () {
+          const message = 'my detail message';
+          logger.detail(message);
+          expect(
+            // Replacing this with a tear-off influences
+            // ignore: unnecessary_lambdas
+            runWithOverrides(() => currentRunLogFile.readAsStringSync()),
+            isNot(contains(message)),
           );
-          expect(readLogFile(), contains('[INFO]'));
-          expect(readLogFile(), contains('MESSAGE'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
+        });
+      });
 
-    test('detail', () {
-      runScoped(
-        () {
-          shorebirdLogger.detail('message');
-          expect(readLogFile(), contains('[DETAIL]'));
-          expect(readLogFile(), contains('message'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
+      group('when log level is lower than debug', () {
+        setUp(() {
+          logger.level = Level.info;
+        });
 
-    test('detail with style', () {
-      runScoped(
-        () {
-          shorebirdLogger.detail(
-            'message',
-            style: (message) => message?.toUpperCase(),
+        test('writes message to log file', () {
+          const message = 'my detail message';
+          logger.detail(message);
+          expect(
+            // Replacing this with a tear-off influences
+            // ignore: unnecessary_lambdas
+            runWithOverrides(() => currentRunLogFile.readAsStringSync()),
+            contains(message),
           );
-          expect(readLogFile(), contains('[DETAIL]'));
-          expect(readLogFile(), contains('MESSAGE'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
-
-    test('warn', () {
-      runScoped(
-        () {
-          shorebirdLogger.warn('message');
-          expect(readLogFile(), contains('[WARN]'));
-          expect(readLogFile(), contains('message'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
-
-    test('warn with style', () {
-      runScoped(
-        () {
-          shorebirdLogger.warn(
-            'message',
-            style: (message) => message?.toUpperCase(),
-          );
-          expect(readLogFile(), contains('[WARN]'));
-          expect(readLogFile(), contains('MESSAGE'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
-
-    test('success', () {
-      runScoped(
-        () {
-          shorebirdLogger.success('message');
-          expect(readLogFile(), contains('[SUCCESS]'));
-          expect(readLogFile(), contains('message'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
-
-    test('success with style', () {
-      runScoped(
-        () {
-          shorebirdLogger.success(
-            'message',
-            style: (message) => message?.toUpperCase(),
-          );
-          expect(readLogFile(), contains('[SUCCESS]'));
-          expect(readLogFile(), contains('MESSAGE'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
-
-    test('alert', () {
-      runScoped(
-        () {
-          shorebirdLogger.alert('message');
-          expect(readLogFile(), contains('[ALERT]'));
-          expect(readLogFile(), contains('message'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
-
-    test('alert with style', () {
-      runScoped(
-        () {
-          shorebirdLogger.alert(
-            'message',
-            style: (message) => message?.toUpperCase(),
-          );
-          expect(readLogFile(), contains('[ALERT]'));
-          expect(readLogFile(), contains('MESSAGE'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
-
-    test('err', () {
-      runScoped(
-        () {
-          shorebirdLogger.err('message');
-          expect(readLogFile(), contains('[ERROR]'));
-          expect(readLogFile(), contains('message'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
-    });
-
-    test('err with style', () {
-      runScoped(
-        () {
-          shorebirdLogger.err(
-            'message',
-            style: (message) => message?.toUpperCase(),
-          );
-          expect(readLogFile(), contains('[ERROR]'));
-          expect(readLogFile(), contains('MESSAGE'));
-        },
-        values: {
-          shorebirdEnvRef.overrideWith(() => shorebirdEnv),
-        },
-      );
+        });
+      });
     });
   });
 }
