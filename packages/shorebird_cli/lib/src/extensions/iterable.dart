eb55d5fb7a77038927beaf93c22ed319455338c3
Bryan Oltman <bryan@shorebird.dev>
2024-07-01 10:22:22 -0400

    simplify containsAnyOf

--- packages/shorebird_cli/lib/src/extensions/iterable.dart
+++ packages/shorebird_cli/lib/src/extensions/iterable.dart
@@ -1,12 +1,5 @@
 /// Provides the [containsAnyOf] method to all [Iterable]s.
 extension ContainsAnyOf<T> on Iterable<T> {
   /// Returns `true` if any of the elements in [elements] are in this iterable.
-  bool containsAnyOf(Iterable<T> elements) {
-    for (final element in elements) {
-      if (contains(element)) {
-        return true;
-      }
-    }
-    return false;
-  }
+  bool containsAnyOf(Iterable<T> elements) => elements.any(contains);
 }
