c2448ed23d65e4e914ede0aa87b1501cea9b1905
Bryan Oltman <bryan@shorebird.dev>
2024-07-12 13:57:10 -0400

    fix(shorebirdl_cli): add stamp file to cache artifacts to mark successful download (#2348)

--- packages/shorebird_cli/lib/src/cache.dart
+++ packages/shorebird_cli/lib/src/cache.dart
@@ -154,11 +154,15 @@ abstract class CachedArtifact {
     return stream.pipe(file.openWrite());
   }
 
+  /// The artifact file on disk.
   File get file =>
       File(p.join(cache.getArtifactDirectory(fileName).path, fileName));
 
+  /// Used to validate that the artifact was fully downloaded and extracted.
+  File get stampFile => File('${file.path}.stamp');
+
   Future<bool> isValid() async {
-    if (!file.existsSync()) {
+    if (!file.existsSync() || !stampFile.existsSync()) {
       return false;
     }
 
@@ -173,6 +177,9 @@ abstract class CachedArtifact {
   }
 
   Future<void> update() async {
+    // Clear any existing artifact files.
+    await _delete();
+
     final request = http.Request('GET', Uri.parse(storageUrl));
     final http.StreamedResponse response;
     try {
@@ -221,6 +228,24 @@ allowed to access $storageUrl.''',
     if (!platform.isWindows && isExecutable) {
       final result = await process.start('chmod', ['+x', file.path]);
       await result.exitCode;
+    }
+
+    _writeStampFile();
+  }
+
+  // Writes a 0-byte file to indicate that the artifact was successfully
+  // installed.
+  void _writeStampFile() {
+    stampFile.createSync(recursive: true);
+  }
+
+  Future<void> _delete() async {
+    if (file.existsSync()) {
+      await file.delete();
+    }
+
+    if (stampFile.existsSync()) {
+      await stampFile.delete();
     }
   }
 }
