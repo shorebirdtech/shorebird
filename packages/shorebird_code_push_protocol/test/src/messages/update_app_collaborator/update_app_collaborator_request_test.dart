0ef8e43b583b89850ca508d5f026970ab7cd2ddf
Bryan Oltman <bryan@shorebird.dev>
2024-09-10 17:38:05 -0400

    chore: remove unnecessary import

--- packages/shorebird_code_push_protocol/test/src/messages/update_app_collaborator/update_app_collaborator_request_test.dart
+++ packages/shorebird_code_push_protocol/test/src/messages/update_app_collaborator/update_app_collaborator_request_test.dart
@@ -1,5 +1,4 @@
 import 'package:shorebird_code_push_protocol/shorebird_code_push_protocol.dart';
-import 'package:shorebird_code_push_protocol/src/messages/update_app_collaborator/update_app_collaborator_request.dart';
 import 'package:test/test.dart';
 
 void main() {
