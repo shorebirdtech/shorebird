c8ffde4524743cbe70a8aaa1bd31db84e6f17d10
Bryan Oltman <bryan@shorebird.dev>
2024-10-25 14:02:03 -0400

    remove unused import

--- packages/shorebird_cli/lib/src/logging/shorebird_logger.dart
+++ packages/shorebird_cli/lib/src/logging/shorebird_logger.dart
@@ -4,7 +4,6 @@ import 'package:mason_logger/mason_logger.dart';
 import 'package:path/path.dart' as p;
 import 'package:scoped_deps/scoped_deps.dart';
 import 'package:shorebird_cli/src/extensions/string.dart';
-import 'package:shorebird_cli/src/logging/logging.dart';
 import 'package:shorebird_cli/src/shorebird_env.dart';
 
 /// A reference to a [Logger] instance.
