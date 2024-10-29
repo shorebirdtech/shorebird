284eb5a1af9a4c6e9195def5ff5905dae779a1cc
Felix Angelov <felix@shorebird.dev>
2024-10-29 15:01:25 -0500

    chore(shorebird_cli): v1.4.8 (#2594)

--- packages/shorebird_cli/lib/src/executables/aot_tools.dart
+++ packages/shorebird_cli/lib/src/executables/aot_tools.dart
@@ -267,7 +267,7 @@ class AotTools {
         if (additionalArgs.isNotEmpty) ...[
           '--',
           ...additionalArgs,
-        ]
+        ],
       ],
       workingDirectory: workingDirectory,
     );
