0159526a285ecd39adb508cbf7c7b9fb66c7da26
Bryan Oltman <bryan@shorebird.dev>
2025-01-03 15:27:18 -0500

    tests

--- packages/shorebird_cli/test/src/executables/powershell_test.dart
+++ packages/shorebird_cli/test/src/executables/powershell_test.dart
@@ -1,6 +1,73 @@
+import 'dart:io';
+
+import 'package:mocktail/mocktail.dart';
+import 'package:scoped_deps/scoped_deps.dart';
 import 'package:shorebird_cli/src/executables/executables.dart';
+import 'package:shorebird_cli/src/shorebird_process.dart';
 import 'package:test/test.dart';
 
+import '../mocks.dart';
+
 void main() {
-  group(Powershell, () {});
+  group(Powershell, () {
+    late ShorebirdProcessResult processResult;
+    late ShorebirdProcess process;
+    late Powershell powershell;
+
+    R runWithOverrides<R>(R Function() body) {
+      return runScoped(
+        () => body(),
+        values: {
+          processRef.overrideWith(() => process),
+        },
+      );
+    }
+
+    setUp(() {
+      processResult = MockShorebirdProcessResult();
+      process = MockShorebirdProcess();
+
+      when(
+        () => process.run(
+          any(),
+          any(),
+          runInShell: any(named: 'runInShell'),
+          workingDirectory: any(named: 'workingDirectory'),
+        ),
+      ).thenAnswer((_) async => processResult);
+
+      powershell = runWithOverrides(Powershell.new);
+    });
+
+    group('getExeVersionString', () {
+      group('when exit code is not success', () {
+        setUp(() {
+          when(() => processResult.exitCode).thenReturn(1);
+        });
+
+        test('throws an exception', () {
+          expect(
+            () => runWithOverrides(
+              () => powershell.getExeVersionString(File('')),
+            ),
+            throwsA(isA<Exception>()),
+          );
+        });
+      });
+
+      group('when exit code is success', () {
+        setUp(() {
+          when(() => processResult.exitCode).thenReturn(0);
+          when(() => processResult.stdout).thenReturn('1.0.0');
+        });
+
+        test('returns the version string', () async {
+          final version = await runWithOverrides(
+            () => powershell.getExeVersionString(File('')),
+          );
+          expect(version, '1.0.0');
+        });
+      });
+    });
+  });
 }
