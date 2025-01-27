3c5cce73476c40169b4f7716ccd0a579553475c6
bryanoltman <bryan@shorebird.dev>
2025-01-27 17:58:22 -0500

    run build_runner

--- packages/shorebird_cli/lib/src/metadata/update_release_metadata.g.dart
+++ packages/shorebird_cli/lib/src/metadata/update_release_metadata.g.dart
@@ -45,7 +45,8 @@ Map<String, dynamic> _$UpdateReleaseMetadataToJson(
 
 const _$ReleasePlatformEnumMap = {
   ReleasePlatform.android: 'android',
-  ReleasePlatform.macos: 'macos',
   ReleasePlatform.ios: 'ios',
+  ReleasePlatform.linux: 'linux',
+  ReleasePlatform.macos: 'macos',
   ReleasePlatform.windows: 'windows',
 };
