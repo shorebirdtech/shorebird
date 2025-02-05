457af4c428c0125b004683b320ac0fdfde45aa0f
Bryan Oltman <bryan@shorebird.dev>
2025-02-05 13:38:00 -0500

    Update packages/shorebird_cli/lib/src/common_arguments.dart
    Co-authored-by: Felix Angelov <felix@shorebird.dev>

--- packages/shorebird_cli/lib/src/common_arguments.dart
+++ packages/shorebird_cli/lib/src/common_arguments.dart
@@ -138,7 +138,7 @@ Bypass all confirmation messages. It's generally not advised to use this unless 
 ''',
   );
 
-  /// An argument that allows the user to bypass interactive confirmations.
+  /// An argument that allows the user to specify a minimum link percentage threshold.
   static const minLinkPercentage = ArgumentDescriber(
     name: 'min-link-percentage',
     defaultValue: '0',
