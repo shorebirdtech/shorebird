d1a67f558c6dee12b9cf2b0a6d5b21419b684cde
Bryan Oltman <bryan@shorebird.dev>
2025-02-11 16:50:41 -0500

    feat: add notes to update release/patch requests

---
+++ packages/shorebird_code_push_protocol/test/src/messages/update_patch/update_patch_request_test.dart
@@ -0,0 +1,16 @@
+import 'package:shorebird_code_push_protocol/shorebird_code_push_protocol.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(UpdatePatchRequest, () {
+    test('can be (de)serialized', () {
+      var request = UpdatePatchRequest(
+        notes: 'notes',
+      );
+      expect(
+        UpdateReleaseRequest.fromJson(request.toJson()).toJson(),
+        equals(request.toJson()),
+      );
+    });
+  });
+}
