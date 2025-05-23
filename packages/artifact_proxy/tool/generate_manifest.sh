fb5136eae81e9809a2ca68fb545c7e80d9b44b79
Felix Angelov <felix@shorebird.dev>
2025-05-23 11:08:16 -0500

    fix(artifact_proxy): include darwin-arm64 artifact overrides

--- packages/artifact_proxy/tool/generate_manifest.sh
+++ packages/artifact_proxy/tool/generate_manifest.sh
@@ -49,6 +49,8 @@ artifact_overrides:
   - flutter_infra_release/flutter/\$engine/android-x64-release/windows-x64.zip
 
   # Dart SDK
+  - flutter_infra_release/flutter/\$engine/darwin-arm64/artifacts.zip
+  - flutter_infra_release/flutter/\$engine/darwin-arm64/font-subset.zip
   - flutter_infra_release/flutter/\$engine/dart-sdk-darwin-arm64.zip
   - flutter_infra_release/flutter/\$engine/dart-sdk-darwin-x64.zip
   - flutter_infra_release/flutter/\$engine/dart-sdk-linux-x64.zip
