246c0f7fd35202a8ccec092831f0dec0052b565c
Felix Angelov <felix@shorebird.dev>
2025-06-02 17:20:55 -0500

    feat(code_push_protocol): make `Patch` extend `Equatable`

--- packages/shorebird_code_push_protocol/test/src/models/patch_test.dart
+++ packages/shorebird_code_push_protocol/test/src/models/patch_test.dart
@@ -6,6 +6,7 @@ void main() {
     test('can be (de)serialized', () {
       const patch = Patch(id: 1, number: 2, notes: 'some notes');
       expect(Patch.fromJson(patch.toJson()).toJson(), equals(patch.toJson()));
+      expect(Patch.fromJson(patch.toJson()), equals(patch));
     });
   });
 }
