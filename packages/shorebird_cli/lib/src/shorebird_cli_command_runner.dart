0c2802362bf9b2363117cb87da7880767eff7f25
Bryan Oltman <bryan@shorebird.dev>
2025-07-03 10:02:14 -0400

    chore(shorebird_cli): remove run command

--- packages/shorebird_cli/lib/src/shorebird_cli_command_runner.dart
+++ packages/shorebird_cli/lib/src/shorebird_cli_command_runner.dart
@@ -81,7 +81,6 @@ class ShorebirdCliCommandRunner extends CompletionCommandRunner<int> {
     addCommand(PreviewCommand());
     addCommand(ReleaseCommand());
     addCommand(ReleasesCommand());
-    addCommand(RunCommand());
     addCommand(UpgradeCommand());
   }
 
