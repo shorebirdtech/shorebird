1195c4c3443d2d559d0cff6be97325cf24762e73
Eric Seidel <eric@shorebird.dev>
2025-11-26 10:04:53 -0800

    fix: shorebird fails when packages use hooks

--- packages/shorebird_cli/test/src/executables/aot_tools_test.dart
+++ packages/shorebird_cli/test/src/executables/aot_tools_test.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:mocktail/mocktail.dart';
 import 'package:path/path.dart' as p;
+import 'package:pub_semver/pub_semver.dart';
 import 'package:scoped_deps/scoped_deps.dart';
 import 'package:shorebird_cli/src/cache.dart';
 import 'package:shorebird_cli/src/executables/executables.dart';
@@ -950,76 +951,139 @@ Run "aot_tools help <command>" for more information about a command.
           expect(result.existsSync(), isTrue);
         });
       });
+    });
 
-      group('getLinkMetadata', () {
-        late int exitCode;
-        late String stdout;
-        late String stderr;
+    group('getVersion', () {
+      late int exitCode;
+      late String stdout;
+      late String stderr;
 
-        setUp(() {
-          stdout = '';
-          stderr = '';
-          exitCode = 0;
+      setUp(() {
+        exitCode = 0;
+        stdout = '';
+        stderr = '';
+        when(
+          () => process.start(
+            any(),
+            any(),
+            workingDirectory: any(named: 'workingDirectory'),
+          ),
+        ).thenAnswer((_) async {
+          final mockProcess = MockProcess();
+          when(() => mockProcess.exitCode).thenAnswer((_) async => exitCode);
           when(
-            () => process.start(
-              any(),
-              any(),
-              workingDirectory: any(named: 'workingDirectory'),
-            ),
-          ).thenAnswer((_) async {
-            final mockProcess = MockProcess();
-            when(() => mockProcess.exitCode).thenAnswer((_) async => exitCode);
-            when(
-              () => mockProcess.stdout,
-            ).thenAnswer((_) => Stream.value(utf8.encode(stdout)));
-            when(
-              () => mockProcess.stderr,
-            ).thenAnswer((_) => Stream.value(utf8.encode(stderr)));
-            return mockProcess;
-          });
+            () => mockProcess.stdout,
+          ).thenAnswer((_) => Stream.value(utf8.encode(stdout)));
           when(
-            () => process.start(
-              any(),
-              any(),
-              workingDirectory: any(named: 'workingDirectory'),
-            ),
-          ).thenAnswer((_) async {
-            final mockProcess = MockProcess();
-            when(() => mockProcess.exitCode).thenAnswer((_) async => exitCode);
-            when(
-              () => mockProcess.stdout,
-            ).thenAnswer((_) => Stream.value(utf8.encode(stdout)));
-            when(
-              () => mockProcess.stderr,
-            ).thenAnswer((_) => Stream.value(utf8.encode(stderr)));
-            return mockProcess;
-          });
+            () => mockProcess.stderr,
+          ).thenAnswer((_) => Stream.value(utf8.encode(stderr)));
+          return mockProcess;
         });
-
-        test(
-          'returns link metadata when aot_tools executes successfully',
-          () async {
-            stdout = '{}';
-            final result = await runWithOverrides(
-              () => aotTools.getLinkMetadata(debugDir: '/debug'),
-            );
-            expect(result, isA<Map<String, dynamic>>());
-          },
-        );
-
-        test(
-          'throws FormatException when aot_tools outputs invalid json',
-          () async {
-            stdout = 'invalid';
-            await expectLater(
-              () => runWithOverrides(
-                () => aotTools.getLinkMetadata(debugDir: '/debug'),
-              ),
-              throwsFormatException,
-            );
-          },
-        );
       });
+
+      test(
+        'returns parsed version when aot_tools outputs valid version',
+        () async {
+          stdout = '1.2.3';
+          final result = await runWithOverrides(() => aotTools.getVersion());
+          expect(result, Version(1, 2, 3));
+        },
+      );
+
+      test('returns 0.0.0 when process exits with non-zero code', () async {
+        exitCode = 1;
+        stderr = 'error';
+        final result = await runWithOverrides(() => aotTools.getVersion());
+        expect(result, Version(0, 0, 0));
+      });
+
+      test('returns 0.0.0 when version string is invalid', () async {
+        stdout = 'invalid version';
+        final result = await runWithOverrides(() => aotTools.getVersion());
+        expect(result, Version(0, 0, 0));
+      });
+
+      test('removes build hooks prefix from version output', () async {
+        stdout = 'Running build hooks...\n1.2.3';
+        final result = await runWithOverrides(() => aotTools.getVersion());
+        expect(result, Version(1, 2, 3));
+      });
+
+      test('handles version with trailing whitespace', () async {
+        stdout = '1.2.3   \n';
+        final result = await runWithOverrides(() => aotTools.getVersion());
+        expect(result, Version(1, 2, 3));
+      });
+    });
+
+    group('getLinkMetadata', () {
+      late int exitCode;
+      late String stdout;
+      late String stderr;
+
+      setUp(() {
+        stdout = '';
+        stderr = '';
+        exitCode = 0;
+        when(
+          () => process.start(
+            any(),
+            any(),
+            workingDirectory: any(named: 'workingDirectory'),
+          ),
+        ).thenAnswer((_) async {
+          final mockProcess = MockProcess();
+          when(() => mockProcess.exitCode).thenAnswer((_) async => exitCode);
+          when(
+            () => mockProcess.stdout,
+          ).thenAnswer((_) => Stream.value(utf8.encode(stdout)));
+          when(
+            () => mockProcess.stderr,
+          ).thenAnswer((_) => Stream.value(utf8.encode(stderr)));
+          return mockProcess;
+        });
+        when(
+          () => process.start(
+            any(),
+            any(),
+            workingDirectory: any(named: 'workingDirectory'),
+          ),
+        ).thenAnswer((_) async {
+          final mockProcess = MockProcess();
+          when(() => mockProcess.exitCode).thenAnswer((_) async => exitCode);
+          when(
+            () => mockProcess.stdout,
+          ).thenAnswer((_) => Stream.value(utf8.encode(stdout)));
+          when(
+            () => mockProcess.stderr,
+          ).thenAnswer((_) => Stream.value(utf8.encode(stderr)));
+          return mockProcess;
+        });
+      });
+
+      test(
+        'returns link metadata when aot_tools executes successfully',
+        () async {
+          stdout = '{}';
+          final result = await runWithOverrides(
+            () => aotTools.getLinkMetadata(debugDir: '/debug'),
+          );
+          expect(result, isA<Map<String, dynamic>>());
+        },
+      );
+
+      test(
+        'throws FormatException when aot_tools outputs invalid json',
+        () async {
+          stdout = 'invalid';
+          await expectLater(
+            () => runWithOverrides(
+              () => aotTools.getLinkMetadata(debugDir: '/debug'),
+            ),
+            throwsFormatException,
+          );
+        },
+      );
     });
   });
 }
