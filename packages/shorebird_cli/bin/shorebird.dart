6bcd20019564949de9742bc5535d2d7c15171cb0
Bryan Oltman <bryan@shorebird.dev>
2025-12-30 17:23:28 -0500

    Add diffRef to main

--- packages/shorebird_cli/bin/shorebird.dart
+++ packages/shorebird_cli/bin/shorebird.dart
@@ -63,6 +63,7 @@ Command: shorebird ${args.join(' ')}
           codePushClientWrapperRef,
           codeSignerRef,
           devicectlRef,
+          diffRef,
           dittoRef,
           doctorRef,
           engineConfigRef,
