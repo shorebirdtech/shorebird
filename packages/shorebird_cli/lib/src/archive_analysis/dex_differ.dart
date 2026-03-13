9d1e351125b6e3bb1b7905d3053250baf54ef919
Eric Seidel <eric@shorebird.dev>
2026-03-13 13:14:07 -0700

    refactor: deduplicate field/method comparison into _compareMembers
    Extract the shared set-diff-and-compare-access-flags pattern into a
    single _compareMembers helper that takes an extraction function and
    kind parameters, eliminating the duplicated _compareFields and
    _compareMethods methods.

--- packages/shorebird_cli/lib/src/archive_analysis/dex_differ.dart
+++ packages/shorebird_cli/lib/src/archive_analysis/dex_differ.dart
@@ -203,48 +203,61 @@ class DexDiffer {
       );
     }
 
-    _compareFields(name, oldClass.classData, newClass.classData, differences);
-    _compareMethods(
-      name,
-      oldClass.classData,
-      newClass.classData,
-      differences,
+    _compareMembers(
+      className: name,
+      oldData: oldClass.classData,
+      newData: newClass.classData,
+      extract: (data) => {
+        for (final f in [...data.staticFields, ...data.instanceFields])
+          '${f.field.className}.${f.field.fieldName}:${f.field.typeName}':
+              f.accessFlags,
+      },
+      memberLabel: 'field',
+      addedKind: DexDifferenceKind.fieldAdded,
+      removedKind: DexDifferenceKind.fieldRemoved,
+      differences: differences,
+    );
+
+    _compareMembers(
+      className: name,
+      oldData: oldClass.classData,
+      newData: newClass.classData,
+      extract: (data) => {
+        for (final m in [...data.directMethods, ...data.virtualMethods])
+          _methodKey(m): m.accessFlags,
+      },
+      memberLabel: 'method',
+      addedKind: DexDifferenceKind.methodAdded,
+      removedKind: DexDifferenceKind.methodRemoved,
+      differences: differences,
     );
   }
 
-  void _compareFields(
-    String className,
-    DexClassData? oldData,
-    DexClassData? newData,
-    List<DexDifference> differences,
-  ) {
-    final oldFields = <String, int>{};
-    final newFields = <String, int>{};
+  /// Compares members (fields or methods) between two class data items.
+  ///
+  /// [extract] converts a [DexClassData] into a map of member key to access
+  /// flags. The key is a human-readable descriptor used in diff descriptions.
+  void _compareMembers({
+    required String className,
+    required DexClassData? oldData,
+    required DexClassData? newData,
+    required Map<String, int> Function(DexClassData) extract,
+    required String memberLabel,
+    required DexDifferenceKind addedKind,
+    required DexDifferenceKind removedKind,
+    required List<DexDifference> differences,
+  }) {
+    final oldMembers = oldData != null ? extract(oldData) : <String, int>{};
+    final newMembers = newData != null ? extract(newData) : <String, int>{};
 
-    if (oldData != null) {
-      for (final f in [...oldData.staticFields, ...oldData.instanceFields]) {
-        final key =
-            '${f.field.className}.${f.field.fieldName}:${f.field.typeName}';
-        oldFields[key] = f.accessFlags;
-      }
-    }
-
-    if (newData != null) {
-      for (final f in [...newData.staticFields, ...newData.instanceFields]) {
-        final key =
-            '${f.field.className}.${f.field.fieldName}:${f.field.typeName}';
-        newFields[key] = f.accessFlags;
-      }
-    }
-
-    final oldKeys = oldFields.keys.toSet();
-    final newKeys = newFields.keys.toSet();
+    final oldKeys = oldMembers.keys.toSet();
+    final newKeys = newMembers.keys.toSet();
 
     for (final added in newKeys.difference(oldKeys)) {
       differences.add(
         DexDifference(
-          kind: DexDifferenceKind.fieldAdded,
-          description: '$className: field added: $added',
+          kind: addedKind,
+          description: '$className: $memberLabel added: $added',
         ),
       );
     }
@@ -252,87 +265,28 @@ class DexDiffer {
     for (final removed in oldKeys.difference(newKeys)) {
       differences.add(
         DexDifference(
-          kind: DexDifferenceKind.fieldRemoved,
-          description: '$className: field removed: $removed',
+          kind: removedKind,
+          description: '$className: $memberLabel removed: $removed',
         ),
       );
     }
 
     for (final common in oldKeys.intersection(newKeys)) {
-      if (oldFields[common] != newFields[common]) {
+      if (oldMembers[common] != newMembers[common]) {
         differences.add(
           DexDifference(
             kind: DexDifferenceKind.accessFlagsChanged,
             description:
-                '$className: field $common access flags changed from '
-                '0x${oldFields[common]!.toRadixString(16)} to '
-                '0x${newFields[common]!.toRadixString(16)}',
+                '$className: $memberLabel $common access flags changed '
+                'from 0x${oldMembers[common]!.toRadixString(16)} '
+                'to 0x${newMembers[common]!.toRadixString(16)}',
           ),
         );
       }
     }
   }
 
-  void _compareMethods(
-    String className,
-    DexClassData? oldData,
-    DexClassData? newData,
-    List<DexDifference> differences,
-  ) {
-    final oldMethods = <String, int>{};
-    final newMethods = <String, int>{};
-
-    if (oldData != null) {
-      for (final m in [...oldData.directMethods, ...oldData.virtualMethods]) {
-        final key = _methodKey(m);
-        oldMethods[key] = m.accessFlags;
-      }
-    }
-
-    if (newData != null) {
-      for (final m in [...newData.directMethods, ...newData.virtualMethods]) {
-        final key = _methodKey(m);
-        newMethods[key] = m.accessFlags;
-      }
-    }
-
-    final oldKeys = oldMethods.keys.toSet();
-    final newKeys = newMethods.keys.toSet();
-
-    for (final added in newKeys.difference(oldKeys)) {
-      differences.add(
-        DexDifference(
-          kind: DexDifferenceKind.methodAdded,
-          description: '$className: method added: $added',
-        ),
-      );
-    }
-
-    for (final removed in oldKeys.difference(newKeys)) {
-      differences.add(
-        DexDifference(
-          kind: DexDifferenceKind.methodRemoved,
-          description: '$className: method removed: $removed',
-        ),
-      );
-    }
-
-    for (final common in oldKeys.intersection(newKeys)) {
-      if (oldMethods[common] != newMethods[common]) {
-        differences.add(
-          DexDifference(
-            kind: DexDifferenceKind.accessFlagsChanged,
-            description:
-                '$className: method $common access flags changed from '
-                '0x${oldMethods[common]!.toRadixString(16)} to '
-                '0x${newMethods[common]!.toRadixString(16)}',
-          ),
-        );
-      }
-    }
-  }
-
-  String _methodKey(DexEncodedMethod m) {
+  static String _methodKey(DexEncodedMethod m) {
     final params = m.method.proto.parameterTypes.join(', ');
     return '${m.method.className}.${m.method.methodName}'
         '($params)${m.method.proto.returnType}';
